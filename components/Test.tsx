f83fff3df65574d7ac0adcd1db108af9bb9f287e
Mai Vang <vmaineng@gmail.com>
2024-04-01 14:50:54 -0700

    added test

---
+++ components/Test.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Button } from "./ui/button";
+import { LucideProps } from "lucide-react";
+import { cn } from "../lib/utils";
+
+type ButtonProps = {
+  variant:
+    | "default"
+    | "secondary"
+    | "outline"
+    | "ghost"
+    | "link"
+    | "destructive";
+  size: "default" | "sm" | "lg" | "icon";
+  className?: string;
+  onClick?: () => void;
+  label?: string;
+  icon?: React.ComponentType<LucideProps & { className?: string }>;
+};
+
+export const CustomButton = ({
+  variant,
+  size,
+  label,
+  icon: IconComponent,
+  className,
+  onClick,
+}: ButtonProps) => {
+  const iconClassName = cn(
+    { "mr-2 h-4 w-4": label && IconComponent },
+    { "h-4 w-4": IconComponent && !label }
+  );
+
+  return (
+    <Button
+      variant={variant}
+      size={size}
+      onClick={onClick}
+      className={className}
+    >
+      {IconComponent && <IconComponent className={iconClassName} />}
+      {label}
+    </Button>
+  );
+};
