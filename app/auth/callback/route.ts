a78f1b7ff9ef6a02cfa6d04ac339b2e5a7a1b025
Jason <jasontorres585@icloud.com>
2024-04-11 22:02:19 +0000

    removed extra comments

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -4,10 +4,5 @@ export async function GET(request: Request) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get('code');
 
-  // If code is present, it means the user has completed the sign-in process
-  // You can handle the sign-in process completion here
-  // For example, you might want to set a cookie or session variable to indicate the user is signed in
-
-  // URL to redirect to after sign-in process completes
   return NextResponse.redirect(requestUrl.origin);
 }
