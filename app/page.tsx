f8afdac834949022c4601e6ce525bd9f299a1340
Jason <jasontorres585@icloud.com>
2024-04-12 18:11:28 -0400

    removed supabase assets. install appwrite (#125)
    ![Screenshot 2024-04-10 at 7 16
    41 PM](https://github.com/LetsGetTechnical/gridiron-survivor/assets/110337114/497c4545-9294-4c97-8441-731b5a69e5b6)
    removed from package.json all supabase
    removed the storybook assets
    removed the supabase.ts files
    renamed the folder to appwrite
    installed appwrite
    
    ---------
    
    Co-authored-by: Shashi Lo <362527+shashilo@users.noreply.github.com>
    Co-authored-by: Mike Chen <1325549+mhchen@users.noreply.github.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,12 @@
-import AuthButton from '../components/AuthButton'
-import { createClient } from '@/utils/supabase/server'
-import { cookies } from 'next/headers'
+import AuthButton from '../components/AuthButton';
 
-export default async function Index() {
-  const cookieStore = cookies()
-
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient(cookieStore)
-      return true
-    } catch (e) {
-      return false
-    }
-  }
-
-  const isSupabaseConnected = canInitSupabaseClient()
-
+export default function Index() {
   return (
-    <div
-      className={[
-        'flex flex-col items-center justify-center flex-1',
-        'w-full',
-      ].join(' ')}
-    >
-      <nav
-        className={[
-          'flex flex-col items-center justify-center flex-1',
-          'w-full',
-        ].join(' ')}
-      >
+    <div className="flex flex-col items-center justify-center flex-1 w-full">
+      <nav className="flex flex-col items-center justify-center flex-1 w-full">
         <p>Gridiron Survivor</p>
-        {isSupabaseConnected ? (
-          <AuthButton />
-        ) : (
-          'Unable to connect to authentication server. Please try again later.'
-        )}
+        <AuthButton />
       </nav>
     </div>
-  )
+  );
 }
