23cda402d135770a7eff821224c4361818a90df3
Ryan Furrer <ryandotfurrer@gmail.com>
2024-04-16 20:20:20 -0400

    Remove note in Developer Guide as per Shashi's request.

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,8 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        // Built-in variants
-        // default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-orange-600 text-white hover:bg-orange-600/90",
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         destructive:
@@ -19,8 +18,6 @@ const buttonVariants = cva(
         "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        // Custom variants
-        default: "bg-orange-600 text-white hover:bg-orange-600/90",
       },
       size: {
         default: "h-10 px-4 py-2",
