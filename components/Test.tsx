c1162afc4807d3df484a592170161fd6ee552528
Jason <jasontorres585@icloud.com>
2024-04-18 19:48:24 +0000

    added folders for src and appwrite. added config.ts and api.ts

--- components/Test.tsx
+++ components/Test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button } from "./ui/button";
 import { LucideProps } from "lucide-react";
-import { cn } from "../lib/utils";
+import { cn } from "../src/lib/utils";
 
 type ButtonProps = {
   variant:
