1ed9d616003922c63e0ae7370cbf0bd64d24e952
Ryan Furrer <ryandotfurrer@gmail.com>
2024-04-19 18:01:20 -0400

    Remove traces of

--- tests/component-tests/Button.test.js
+++ tests/component-tests/Button.test.js
@@ -4,7 +4,7 @@ import '@testing-library/jest-dom';
 import { Button } from '@/components/ui/Button';
 
 describe('Button', () => {
-  test('Renders default (primary) button', () => {
+  test('Renders button and checks for className that mages the background of it, thus confirming the default button rendered.', () => {
     render(<Button/>);
     const defaultButtton = document.querySelector('.bg-orange-600')
     expect(defaultButtton).toBeInTheDocument();
