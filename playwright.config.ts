c00505da383491352137dc27d9c8373aa877c9da
Mai Vang <vmaineng@gmail.com>
2024-04-20 09:31:50 -0700

    added in match to specficiy playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,6 +17,8 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+  
+  testMatch: '**/*.spec.ts',
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
