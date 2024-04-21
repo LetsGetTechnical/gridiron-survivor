a45f98f9058c181747891517a7dcf59a08c1ee15
Mai Vang <vmaineng@gmail.com>
2024-04-21 13:54:36 -0700

    formatted files

--- api/authFunctions.ts
+++ api/authFunctions.ts
@@ -12,26 +12,3 @@ import { useRouter } from 'next/navigation';
     }
   };
 
-  export const register = async (name: string, email: string, password: string) => {
-    // setLoading(true);
-    try {
-      const newUser = await account.create(name, email, password);
-      console.log('User registered successfully:', newUser);
-      router.refresh(); // or any other redirection logic
-  } catch (error) {
-      console.error('Error registering user:', error);
-      alert('Failed to register user. Please try again.');
-  }
-  };
-
-   export const login = async (email: string, password: string) => {
-    // console.log(login)
-    try {
-        await account.createEmailPasswordSession(email, password)
-        console.log(email, password)
-        router.push('/')
-    } catch (error) {
-        console.log(error)
-        alert(error)
-    }
-}
