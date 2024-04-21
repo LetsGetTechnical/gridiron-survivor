e32f32fdf26cbaf92af279d031c6ea58f83c1a38
Mai Vang <vmaineng@gmail.com>
2024-04-21 14:01:29 -0700

    modified useRouter

--- api/authFunctions.ts
+++ api/authFunctions.ts
@@ -1,27 +1,16 @@
 import {account } from "./appwrite"
 import { useRouter } from 'next/navigation';
 
-  const router = useRouter();
+ 
 
    export const logout = async () => {
     try {
       await account.deleteSession('current');
+      const router = useRouter()
       router.push('/');
     } catch (error) {
       console.error(error);
     }
-  };
-
-  export const register = async (name: string, email: string, password: string) => {
-    // setLoading(true);
-    try {
-      const newUser = await account.create(name, email, password);
-      console.log('User registered successfully:', newUser);
-      router.refresh(); // or any other redirection logic
-  } catch (error) {
-      console.error('Error registering user:', error);
-      alert('Failed to register user. Please try again.');
-  }
   };
 
    export const login = async (email: string, password: string) => {
@@ -29,6 +18,7 @@ import { useRouter } from 'next/navigation';
     try {
         await account.createEmailPasswordSession(email, password)
         console.log(email, password)
+        const router = useRouter()
         router.push('/')
     } catch (error) {
         console.log(error)
