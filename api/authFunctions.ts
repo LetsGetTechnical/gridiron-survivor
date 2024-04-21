272935b78cb040872b5311837e60f9d846a5e2de
Mai Vang <vmaineng@gmail.com>
2024-04-21 11:01:19 -0700

    modified folders for appwrite apis

--- api/authFunctions.ts
+++ api/authFunctions.ts
@@ -1,10 +1,9 @@
-import {account, ID } from "./appwrite"
+import {account, ID } from "./config"
 import { useRouter } from 'next/navigation';
 import { useState, ChangeEvent } from "react";
 
 const useAuthFunctions = () => {
   const router = useRouter();
-  const [loading, setLoading] = useState(false);
 
   const logout = async () => {
     try {
@@ -15,33 +14,7 @@ const useAuthFunctions = () => {
     }
   };
 
-  const register = async (name: string, email: string, password: string) => {
-    setLoading(true);
-    try {
-      const newUser = await account.create(name, email, password);
-      console.log('User registered successfully:', newUser);
-      router.refresh(); // or any other redirection logic
-  } catch (error) {
-      console.error('Error registering user:', error);
-      alert('Failed to register user. Please try again.');
-  } finally {
-      setLoading(false);
-  }
-  };
-
-  const login = async (email: string, password: string) => {
-    // console.log(login)
-    try {
-        await account.createEmailPasswordSession(email, password)
-        console.log(email, password)
-        router.push('/dashboard')
-    } catch (error) {
-        console.log(error)
-        alert(error)
-    }
-}
-
-  return { logout, register, loading, login};
+  return { logout};
 };
 
 export default useAuthFunctions;
