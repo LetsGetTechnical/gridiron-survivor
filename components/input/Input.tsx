763dca923dd0a4f0607a02d21437fe0cbd04902d
Emmilie Estabillo <emsestabillo@gmail.com>
2024-04-22 16:18:31 -0400

    Remove destructured props

--- components/input/Input.tsx
+++ components/input/Input.tsx
@@ -8,14 +8,12 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, value, onChange, ...props }, ref) => {
     return (
       <input
-        value={value}
-        onChange={onChange}
+        {...props}
         className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base text-zinc-900 border-zinc-300 placeholder:border-zinc-400 placeholder:font-normal ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base text-zinc-900 border-zinc-300 placeholder:border-zinc-400 placeholder:font-normal ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
-        {...props}
       />
     )
   }
