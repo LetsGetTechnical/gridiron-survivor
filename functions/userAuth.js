c275fc9949478735eda199dd6932992d018f5a72
Richard Choi <richardchoi54@gmail.com>
2024-04-22 19:25:39 -0400

    add create user on post request

--- functions/userAuth.js
+++ functions/userAuth.js
@@ -1,5 +1,39 @@
+const sdk = require('node-appwrite');
+const { ID } = require("node-appwrite");
+
 export default async ({ req, res, log, error }) => {
-    if (req.method == "GET") {
-        return res.send("Function was updated!");
+
+    // Init SDK
+    const client = new sdk.Client();
+
+    const databases = new sdk.Databases(client);
+
+    client
+        .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
+        .setProject(process.env.PROJECT_ID) // Your project ID
+        .setKey(process.env.X - Appwrite - Key) // Your secret API key
+
+    // get all users
+    if (req.method === "GET") {
+
     }
+
+    // update users <user data>
+
+    if (req.method === "PUT") {
+
+    }
+
+    // create a new user
+    if (req.method === "POST") {
+        await databases.createDocument(process.env.DATABASE_ID, process.env.COLLECTION_USERS_ID, ID.unique(), {});
+        res.json({ msg: "User was created successfully!" });
+    }
+
+    // delete a user
+
+    if (req.method === "DELETE") {
+
+    }
+    console.log()
 };
