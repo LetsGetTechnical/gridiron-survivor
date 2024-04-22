eccbd29e27bf70d717e3e45441ef715359f08268
Emmilie Estabillo <emsestabillo@gmail.com>
2024-04-22 11:42:36 -0400

    Remove placeholder and type props, add value and onchange

--- components/input/Input.tsx
+++ components/input/Input.tsx
@@ -5,11 +5,11 @@ export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ placeholder, className, type, ...props }, ref) => {
+  ({ className, value, onChange, ...props }, ref) => {
     return (
       <input
-        type={type}
-        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
         className={cn(
           "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base text-zinc-900 border-zinc-300 placeholder:border-zinc-400 placeholder:font-normal ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
           className
