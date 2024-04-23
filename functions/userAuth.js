c54c0ed7b3828717f4b100b02fd3bf7da2382b12
Richard Choi <richardchoi54@gmail.com>
2024-04-23 14:51:56 -0400

    change $id to brackets and add context.log

--- functions/userAuth.js
+++ functions/userAuth.js
@@ -15,13 +15,13 @@ const user = async ({ req, res, log, error }) => {
 
     // create a new user
     if (req.method === "POST") {
-
+        context.log(req.body.$id)
         await databases.createDocument(process.env.DATABASE_ID, process.env.COLLECTION_USERS_ID, ID.unique(), {
             "email": req.body.email,
             "name": req.body.name,
             "password": req.body.pasword,
             "labels": req.body.labels,
-            "userId": req.body.$id
+            "userId": req.body['$id']
         });
         return res.json({ msg: "User was created successfully!" });
     }
