85c028a10b0fe70590048920933def9fefc613b8
Richard <66279068+choir27@users.noreply.github.com>
2024-04-24 12:56:26 -0400

    User DB collection w/ Appwrite Funcs (#152)
    Using Appwrite Functions, implemented code that received a POST request,
    and upon hearing that POST request, would successfully store the given
    user data into the created user DB collection.
    
    Also since Appwrite Functions already includes env variables inside of
    the Appwrite console, there was no need to place any of the env
    variables in the code for this to work successfully.
    
    Successful POST execution
    ![Screenshot 2024-04-22
    204824](https://github.com/LetsGetTechnical/gridiron-survivor/assets/66279068/7a14914c-59ab-4605-b8b7-405a2539dd19)
    
    User data sucessfully stored in DB collection
    ![Screenshot 2024-04-22
    204712](https://github.com/LetsGetTechnical/gridiron-survivor/assets/66279068/b3e93444-1593-43fb-a3b1-1d63839fa6e9)
    
    Appwrite function successfully created
    ![Screenshot 2024-04-22
    205016](https://github.com/LetsGetTechnical/gridiron-survivor/assets/66279068/2b7fc9c2-b1b2-4622-b102-827a334ff5fb)
    
    ---------
    
    Co-authored-by: Shashi Lo <362527+shashilo@users.noreply.github.com>

---
+++ functions/userAuth.js
@@ -0,0 +1,29 @@
+const { ID } = require("appwrite");
+const sdk = require("node-appwrite")
+
+const user = async ({ req, res, log, error }) => {
+
+    // Init SDK
+    const client = new sdk.Client();
+
+    const databases = new sdk.Databases(client);
+
+    client
+        .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
+        .setProject(process.env.PROJECT_ID) // Your project ID
+        .setKey(process.env.X_Appwrite_Key) // Your secret API key
+
+    // create a new user
+    if (req.method === "POST" && req.body.email && req.body.email && req.body['$id']) {
+        await databases.createDocument(process.env.DATABASE_ID, process.env.COLLECTION_USERS_ID, ID.unique(), {
+            "email": req.body.email,
+            "name": req.body.name,
+            "labels": req.body.labels,
+            "userId": req.body['$id']
+        });
+        return res.json({ msg: "User was created successfully!" });
+    }
+
+};
+
+module.exports = user;
