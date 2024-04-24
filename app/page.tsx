e3bf2aa8aecdfac19a68b70ecc9588751b770338
Ryan Furrer <ryandotfurrer@gmail.com>
2024-04-24 18:18:04 -0400

    Remove references to AuthButton

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
-import AuthButton from "@/components/authbutton/AuthButton";
-
 export default function Index() {
   return (
     <div className="flex flex-col items-center justify-center flex-1 w-full">
       <nav className="flex flex-col items-center justify-center flex-1 w-full">
         <p className='pb-4'>Gridiron Survivor</p>
-        <AuthButton />
       </nav>
     </div>
   );
