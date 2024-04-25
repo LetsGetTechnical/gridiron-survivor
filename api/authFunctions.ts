98279e959118269bb0376ad57129d8742d7c850b
Richard Choi <richardchoi54@gmail.com>
2024-04-25 17:01:46 -0400

    replace json fetch with appwrite sdk inbuilt listdocuments method

--- api/authFunctions.ts
+++ api/authFunctions.ts
@@ -1,12 +1,17 @@
+import { Client, Databases } from "appwrite";
+
 export async function getWeeklyPicks() {
     try {
-        return await fetch(`https://cloud.appwrite.io/v1/databases/${process.env.DATABASE_ID}/collections/${process.env.WEEKLY_PICKS_COLLECTION_ID}/documents`, {
-            method: "GET", // *GET, POST, PUT, DELETE, etc.
-            headers: {
-                "Content-Type": "application/json",
-                "X-Appwrite-Project": "6616ea581ef9f5521c7d",
-            },
-        })
+        const client = new Client();
+
+        const databases = new Databases(client);
+
+        client
+            .setEndpoint("https://cloud.appwrite.io/v1")
+            .setProject("6616ea581ef9f5521c7d");
+
+        return await databases.listDocuments("6616ea9dcb86b246a7b0", "6622aa4d2db201231e31");
+
     } catch (err) {
         console.error(err);
     }
