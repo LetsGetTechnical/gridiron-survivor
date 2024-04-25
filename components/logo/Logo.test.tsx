94e8f076022c3e6f55c25096a70c9b731a749f48
Emmilie Estabillo <emsestabillo@gmail.com>
2024-04-24 22:21:25 -0400

    Remove mock, add image src to resolve error

--- components/logo/Logo.test.tsx
+++ components/logo/Logo.test.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import Logo from './Logo';
-import { ImageProps } from 'next/image';
-
-jest.mock('next/image', () => ({
-  __esModule: true,
-  default: (props: ImageProps) => {
-    return <img {...props} src={props.src as string} />;
-  },
-}));
 
 describe('Logo', () => {
   it('renders correctly', () => {
-    const { getByAltText } = render(<Logo />);
+    const { getByAltText } = render(
+      <Logo src="/assets/logo-colored-outline.svg" />,
+    );
     expect(getByAltText('Gridiron Survivor logo')).toBeInTheDocument();
   });
 });
