2c7e6fbc982f99ee619702cba6cd1c1d4636ef73
Mai Vang <vmaineng@gmail.com>
2024-04-25 12:10:40 -0700

    reverted login/page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,11 @@
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
-export default function Login() {
+export default function Login({
+  searchParams,
+}: {
+  searchParams: { message: string };
+}) {
   return (
     <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
       <h1 className="text-2xl font-bold text-center text-foreground">Hello GIS</h1>
