82ca76d2211960ee2e0ab6c2c78dc71448b6cdb8
Ryan Furrer <ryandotfurrer@gmail.com>
2024-04-26 17:52:09 -0400

    Remove test files for Nav and NavDrawer components as we will add them after the MVP deadline is complete

--- components/button/Button.test.tsx
+++ components/button/Button.test.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { Button } from './Button';
 
-it('should render a button with no variant defined', () => {
+test('should render a button with no variant defined', () => {
   render(<Button />);
   const defaultButton = screen.getByRole('button');
   expect(defaultButton).toHaveClass('bg-orange-600');
 });
 
-it('should render a button with the link variant defined. It should have no background or border, only an underline on hover', () => {
+test('should render a button with the link variant defined. It should have no background or border, only an underline on hover', () => {
   render(<Button variant="link" />);
   const defaultButton = screen.getByRole('button');
   expect(defaultButton).toHaveClass('underline-offset-4');
