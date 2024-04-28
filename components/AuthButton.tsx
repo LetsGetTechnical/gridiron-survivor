6c901e1264baafa089a04fd1c1c0d378a458fcd2
Ryan Furrer <ryandotfurrer@gmail.com>
2024-04-28 11:46:54 -0400

    Remove commented out code in AuthButton and un-comment out prettierIgnore file

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -3,9 +3,7 @@
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
 
-
 export default function AuthButton() {
- 
   //Assume user is always logged in for demonstration purposes
   const user = { email: 'example@example.com' };
 
@@ -15,22 +13,24 @@ export default function AuthButton() {
     return redirect('/login');
   };
 
-
   return user ? (
     <div className="flex items-center gap-4">
       Hey, {user.email}!
-      {/* <form onSubmit={signOut}> */}
-        <button type="submit" className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
-          Logout
-        </button>
-      {/* </form> */}
+      <button
+        type="submit"
+        className="bg-btn-background hover:bg-btn-background-hover rounded-md px-4 py-2 no-underline"
+      >
+        Logout
+      </button>
     </div>
   ) : (
-    <Link href="/login" className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
+    <Link
+      href="/login"
+      className="bg-btn-background hover:bg-btn-background-hover flex rounded-md px-3 py-2 no-underline"
+    >
       Login
     </Link>
-      );
+  );
 }
 
 //  export default AuthButton;
-
