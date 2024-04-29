ec8f3f137df0e2b67ebb88d27609447343a81bb0
Emmilie Estabillo <emsestabillo@gmail.com>
2024-04-29 18:05:21 -0400

    Remove static team logos and replace with placeholder, update story

--- components/WeeklyPickButton/WeeklyPickButton.stories.tsx
+++ components/WeeklyPickButton/WeeklyPickButton.stories.tsx
@@ -25,14 +25,14 @@ type Story = StoryObj<typeof meta>;
 const withRadioGroup: Decorator = (Story) => (
   <RadioGroup>
     <Story />
-    <WeeklyPickButton team="Cowboys" src="/assets/team-cowboys.svg" />
+    <WeeklyPickButton team="Cowboys" src="/assets/team-logo-placeholder.jpg" />
   </RadioGroup>
 );
 
 export const Primary: Story = {
   args: {
     team: 'Ravens',
-    src: '/assets/team-ravens.svg',
+    src: '/assets/team-logo-placeholder.jpg',
   },
   decorators: withRadioGroup,
 };
