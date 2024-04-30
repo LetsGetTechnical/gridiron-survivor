fff3d946fa5b798066c3c47d3dcdd2de41130f63
Ryan Furrer <ryandotfurrer@gmail.com>
2024-04-29 21:08:08 -0400

    Create nav component (#158)
    Closes #155 
    
    # Created nav component
    ![Screenshot of Nav component in dark
    mode](https://github.com/LetsGetTechnical/gridiron-survivor/assets/40150036/954db134-5f79-47d3-aa6f-df152bf9b4b8)
    
    
    https://github.com/LetsGetTechnical/gridiron-survivor/assets/40150036/0e143fbb-a8f7-45b9-9465-0983c3dfbc10
    
    ## Todo
    - [x] Add hamburger icon
    ~~- [ ] Add logic to show sign out only if user is signed in (?)~~ Not
    needed for MVP
    - [x] Integrate `Button.tsx` component from #132 and use in place of
    standard `<button>` element
    
    ---------
    
    Co-authored-by: Shashi Lo <362527+shashilo@users.noreply.github.com>

---
+++ components/Nav/Nav.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import LogoNav from '../LogoNav/LogoNav';
+import { Menu } from 'lucide-react';
+import { Button } from '../Button/Button';
+import {
+  Drawer,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '../NavDrawer/NavDrawer';
+
+export const Nav = () => {
+  return (
+    <nav className="flex h-16 items-center border-b border-zinc-100 from-[#4E160E] to-zinc-950 px-4 dark:border-zinc-800 dark:bg-gradient-to-b">
+      <div className="mr-auto">
+        <LogoNav />
+      </div>
+      <ul>
+        <li>
+          <Drawer>
+            <DrawerTrigger>
+              <Menu />
+            </DrawerTrigger>
+            <DrawerContent>
+              <DrawerHeader>
+                <DrawerTitle>Gridiron Survivor</DrawerTitle>
+              </DrawerHeader>
+              <ul className="m-0 flex flex-col gap-4 p-0">
+                <li>
+                  <Button
+                    className="p-0 text-base font-normal text-zinc-600"
+                    variant="link"
+                    label="Sign Out"
+                  />
+                </li>
+              </ul>
+            </DrawerContent>
+          </Drawer>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
---
+++ components/NavDrawer/NavDrawer.tsx
@@ -0,0 +1,128 @@
+'use client';
+
+import * as React from 'react';
+import { Drawer as DrawerPrimitive } from 'vaul';
+import { X } from 'lucide-react';
+
+import { cn } from '@/lib/utils';
+
+const Drawer = ({
+  shouldScaleBackground = true,
+  ...props
+}: React.ComponentProps<typeof DrawerPrimitive.Root>) => (
+  <DrawerPrimitive.Root
+    shouldScaleBackground={shouldScaleBackground}
+    direction="right"
+    {...props}
+  />
+);
+Drawer.displayName = 'Drawer';
+
+const DrawerTrigger = DrawerPrimitive.Trigger;
+
+const DrawerPortal = DrawerPrimitive.Portal;
+
+const DrawerClose = DrawerPrimitive.Close;
+
+const DrawerOverlay = React.forwardRef<
+  React.ElementRef<typeof DrawerPrimitive.Overlay>,
+  React.ComponentPropsWithoutRef<typeof DrawerPrimitive.Overlay>
+>(({ className, ...props }, ref) => (
+  <DrawerPrimitive.Overlay
+    ref={ref}
+    className={cn('fixed inset-0 z-50 bg-black/80', className)}
+    {...props}
+  />
+));
+DrawerOverlay.displayName = DrawerPrimitive.Overlay.displayName;
+
+const DrawerContent = React.forwardRef<
+  React.ElementRef<typeof DrawerPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof DrawerPrimitive.Content>
+>(({ className, children, ...props }, ref) => (
+  <DrawerPortal>
+    <DrawerOverlay />
+    <DrawerPrimitive.Content
+      ref={ref}
+      className={cn(
+        'fixed inset-x-0 bottom-0 left-auto right-0 top-0 z-50 mt-0 flex h-auto w-4/5 flex-col gap-4 rounded-none border-l border-zinc-100 bg-background px-4 dark:border-zinc-800 md:w-2/5 lg:w-1/5',
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </DrawerPrimitive.Content>
+  </DrawerPortal>
+));
+DrawerContent.displayName = 'DrawerContent';
+
+const DrawerHeader = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={cn(
+      'grid h-16 items-center gap-1.5 px-0 text-center sm:text-left',
+      className,
+    )}
+    {...props}
+  />
+);
+DrawerHeader.displayName = 'DrawerHeader';
+
+const DrawerFooter = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={cn('mt-auto flex flex-col gap-2 p-4', className)}
+    {...props}
+  />
+);
+DrawerFooter.displayName = 'DrawerFooter';
+
+const DrawerTitle = React.forwardRef<
+  React.ElementRef<typeof DrawerPrimitive.Title>,
+  React.ComponentPropsWithoutRef<typeof DrawerPrimitive.Title>
+>(({ className, ...props }, ref) => (
+  <div className="flex items-center">
+    <DrawerPrimitive.Title
+      ref={ref}
+      className={cn(
+        'mr-auto text-lg font-semibold leading-none tracking-tight',
+        className,
+      )}
+      {...props}
+    />
+
+    <DrawerClose>
+      <X />
+    </DrawerClose>
+  </div>
+));
+DrawerTitle.displayName = DrawerPrimitive.Title.displayName;
+
+const DrawerDescription = React.forwardRef<
+  React.ElementRef<typeof DrawerPrimitive.Description>,
+  React.ComponentPropsWithoutRef<typeof DrawerPrimitive.Description>
+>(({ className, ...props }, ref) => (
+  <DrawerPrimitive.Description
+    ref={ref}
+    className={cn('text-sm text-muted-foreground', className)}
+    {...props}
+  />
+));
+DrawerDescription.displayName = DrawerPrimitive.Description.displayName;
+
+export {
+  Drawer,
+  DrawerPortal,
+  DrawerOverlay,
+  DrawerTrigger,
+  DrawerClose,
+  DrawerContent,
+  DrawerHeader,
+  DrawerFooter,
+  DrawerTitle,
+  DrawerDescription,
+};
