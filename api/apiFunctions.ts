5d0dcb30331afdd02f97d87c2f9a7c2c4667c8a3
choir27 <richardchoi54@gmail.com>
2024-04-29 21:18:37 -0400

    remove databases from apiFunctions.ts and move it to config.ts

--- api/apiFunctions.ts
+++ api/apiFunctions.ts
@@ -1,5 +1,4 @@
-import { account } from "./config"
-import { Client, Databases } from "appwrite";
+import { account, databases } from "./config"
 
 export async function loginAccount(user: { email: string; password: string }) {
   try {
@@ -19,16 +18,7 @@ export async function logoutAccount() {
 
 export async function getWeeklyPicks() {
     try {
-        const client = new Client();
-
-        const databases = new Databases(client);
-
-        client
-            .setEndpoint("https://cloud.appwrite.io/v1")
-            .setProject("6616ea581ef9f5521c7d");
-
         return await databases.listDocuments("6616ea9dcb86b246a7b0", "6622aa4d2db201231e31");
-
     } catch (err) {
         console.error(err);
     }
