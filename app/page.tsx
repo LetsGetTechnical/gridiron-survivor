ae651556bf12b86fcbe8e3830846900fd91d8e87
choir27 <richardchoi54@gmail.com>
2024-04-30 15:09:29 -0400

    fix page.tsx div/nav tags

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,6 @@ export default function Index() {
   return (
     <div className="flex flex-col items-center justify-center flex-1 w-full">
       <nav className="flex flex-col items-center justify-center flex-1 w-full">
-    <div className="flex flex-col items-center justify-center flex-1 w-full">
-      <nav className="flex flex-col items-center justify-center flex-1 w-full">
         <p>Gridiron Survivor</p>
       </nav>
     </div>
