7ace5e579d79acebe644023b5026fa022aaaa5d8
Mai Vang <vmaineng@gmail.com>
2024-04-30 11:39:26 -0700

    added in login functionality

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,15 +1,35 @@
+'use client';
+import { useState, ChangeEvent } from 'react';
 import Link from 'next/link';
-import { redirect } from 'next/navigation';
+import { redirect, useRouter } from 'next/navigation';
 import Logo from '@/components/logo/Logo';
 import logo from '/public/assets/logo-colored-outline.svg';
 import { Input } from '@/components/input/Input';
 import { Button } from '@/components/Button/Button';
+import { loginAccount } from '@/api/apiFunctions';
 
 export default function Login({
   searchParams,
 }: {
   searchParams: { message: string };
 }) {
+  const router = useRouter();
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleEmail = (e: ChangeEvent<HTMLInputElement>): void => {
+    setEmail(e.target.value);
+  };
+
+  const handlePassword = (e: ChangeEvent<HTMLInputElement>): void => {
+    setPassword(e.target.value);
+  };
+
+  const handleLogin = () => {
+    loginAccount({ email, password });
+    router.push('/weeklyPicks');
+  };
+
   return (
     <div className="h-screen w-full">
       <div className="grid h-screen w-full grid-cols-2 bg-gradient-to-b from-[#4E160E] to-zinc-950">
@@ -31,11 +51,29 @@ export default function Login({
               Join Gridiron Survivor
             </h1>
             <p className="pb-4 font-normal leading-7 text-zinc-500">
-              Log in to your existing account or sign up to get started with a
-              league
+              <Link href="/register">
+                Log in to your existing account or sign up to get started with a
+                league
+              </Link>
             </p>
-            <Input placeholder="Email" />
-            <Button label="Continue" />
+            <Input
+              type="email"
+              value={email}
+              placeholder="Email"
+              onChange={handleEmail}
+            />
+            <Input
+              type="password"
+              value={password}
+              placeholder="Password"
+              onChange={handlePassword}
+            />
+            <Button
+              label="Continue"
+              disabled={!email || !password}
+              onClick={handleLogin}
+            />
+            <Link href="/register">Sign up to get started with a league</Link>
           </div>
         </div>
       </div>
---
+++ app/weeklyPicks/page.tsx
@@ -0,0 +1,9 @@
+export default function weeklyPicks() {
+  return (
+    <div className="grid place-content-center bg-white p-8">
+      <h1 className="text-5xl font-extrabold tracking-tight text-foreground">
+        Weekly Picks
+      </h1>
+    </div>
+  );
+}
