56611ac0e816684288cd7ced28fd52b85a331ed3
choir27 <richardchoi54@gmail.com>
2024-05-01 16:45:36 -0400

    after team select on refresh, orange border on respective team selection persists on refresh

--- app/weeklyPicks/page.tsx
+++ app/weeklyPicks/page.tsx
@@ -1,9 +1,14 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Models } from 'appwrite';
 import { WeeklyPickButton } from '../../components/WeeklyPickButton/WeeklyPickButton';
 import { RadioGroup } from '../../components/RadioGroup/RadioGroup';
 import { Button } from '../../components/Button/Button';
-import { getUserWeeklyPick, createWeeklyPicks, getNFLTeams } from '@/api/apiFunctions';
+import {
+  getUserWeeklyPick,
+  createWeeklyPicks,
+  getNFLTeams,
+} from '@/api/apiFunctions';
 import { account } from '../../api/config';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
@@ -27,52 +32,84 @@ const FormSchema = z.object({
 });
 
 export default function WeeklyPickForm() {
-  // {gameId: string, gameWeekId: string, userResults: string}
+  const [NFLTeams, setNFLTeams] = useState<Models.Document[]>([]);
+  const [weeklyPicks, setWeeklyPicks] = useState<Models.Document>();
+
+  useEffect(() => {
+    async function fetchNFLTeams() {
+      const data = await getNFLTeams();
+      if (data) {
+        const response = data.documents;
+        setNFLTeams(response);
+      }
+    }
+
+    fetchNFLTeams();
+  }, []);
+
+  useEffect(() => {
+    async function fetchWeeklyPicks() {
+      const user = await account.get();
+
+      const userPicks = await getUserWeeklyPick({
+        userId: user.$id,
+        weekNumber: '6622c75658b8df4c4612',
+      });
+
+      const nflTeams = await getNFLTeams();
+
+      if (nflTeams && userPicks) {
+        const userResponse = userPicks;
+        const nflResponse = nflTeams.documents;
+
+        const findTeam = nflResponse.find(
+          (team) => team.$id === userResponse[user.$id].team,
+        );
+
+        setWeeklyPicks(findTeam);
+      }
+    }
+
+    fetchWeeklyPicks();
+  }, []);
 
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
   });
 
   async function onSubmit(data: z.infer<typeof FormSchema>) {
-    const teamSelect = data.type.toLowerCase();
+    try {
+      const teamSelect = data.type.toLowerCase();
 
-    let correct = false;
+      localStorage.setItem('team', data.type);
+      let correct = false;
 
-    if(teamSelect === "vikings"){
-      correct = true;
-    }else if(teamSelect === "cowboys"){
-      correct = false;
+      if (teamSelect === 'vikings') {
+        correct = true;
+      } else if (teamSelect === 'cowboys') {
+        correct = false;
+      }
+
+      const user = await account.get();
+      const response = await getUserWeeklyPick({
+        userId: user.$id,
+        weekNumber: '6622c75658b8df4c4612',
+      });
+
+      const findTeamID = NFLTeams.find(
+        (ele) => ele.teamName.toLowerCase() === teamSelect,
+      );
+
+      response[user.$id] = { team: findTeamID?.$id, correct: correct };
+
+      createWeeklyPicks({
+        gameId: '66311a210039f0532044',
+        gameWeekId: '6622c7596558b090872b',
+        userResults: JSON.stringify(response),
+      });
+    } catch (err) {
+      console.error(err);
     }
-
-    const teams = await getNFLTeams();
-
-    if(teams){
-      
-    const user = await account.get();
-    const response = await getUserWeeklyPick({
-      userId: user.$id,
-      weekNumber: '6622c75658b8df4c4612',
-    });
-
-    console.log(response[user.$id]);
-
-      const findTeamID = teams.documents.find((ele)=>ele.teamName===teamSelect);
-
-      response[user.$id] = {"team": findTeamID?.$id, "correct": correct};
-
-    const weeklyPick = {
-      gameId: "66311a210039f0532044",
-      gameWeekId: "6622c7596558b090872b",
-      userResults: JSON.stringify(response)
-    };
-
-    createWeeklyPicks(weeklyPick);
-  }
-
-
-
-    // createWeeklyPicks();
-    // console.log(document.querySelectorAll('[aria-label]'));
   }
 
   return (
@@ -93,21 +130,26 @@ export default function WeeklyPickForm() {
                 <FormControl>
                   <RadioGroup
                     onValueChange={field.onChange}
-                    defaultValue={field.value}
+                    defaultValue={
+                      weeklyPicks
+                        ? weeklyPicks.teamName
+                        : localStorage.getItem('team')
+                    }
                   >
                     <FormItem>
                       <FormControl>
                         <WeeklyPickButton
-                          team="Vikings"
-                          src="https://cdn.worldvectorlogo.com/logos/minnesota-vikings-2.svg"
+                          team={NFLTeams[0]?.teamName}
+                          src={NFLTeams[0]?.teamLogo}
                         />
                       </FormControl>
                     </FormItem>
+
                     <FormItem>
                       <FormControl>
                         <WeeklyPickButton
-                          team="Cowboys"
-                          src="https://cdn.worldvectorlogo.com/logos/dallas-cowboys-1.svg"
+                          team={NFLTeams[1]?.teamName}
+                          src={NFLTeams[1]?.teamLogo}
                         />
                       </FormControl>
                     </FormItem>
