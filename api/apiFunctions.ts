773688c859040d4e0c6929e8043803dbeb8e3e7c
Clue355 <dm.carlosluevano@gmail.com>
2024-04-30 19:16:41 -0500

    registration page

--- api/apiFunctions.ts
+++ api/apiFunctions.ts
@@ -5,6 +5,7 @@ export async function loginAccount(user: { email: string; password: string }) {
     return await account.createEmailPasswordSession(user.email, user.password);
   } catch (error) {
     console.error(error);
+    throw error;
   }
 }
 
@@ -16,13 +17,19 @@ export async function logoutAccount() {
   }
 }
 
-export async function getUserWeeklyPick(data: {userId: string, weekNumber: string}) {
-    try {
-        const response = await databases.listDocuments(appwriteConfig.databaseId, "66313025000612a5380e");
-        return JSON.parse(response.documents[0].userResults)[data.userId];
-    } catch (err) {
-        console.error(err);
-    }
+export async function getUserWeeklyPick(data: {
+  userId: string;
+  weekNumber: string;
+}) {
+  try {
+    const response = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      '66313025000612a5380e',
+    );
+    return JSON.parse(response.documents[0].userResults)[data.userId];
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 export async function registerAccount(request: {
@@ -35,14 +42,23 @@ export async function registerAccount(request: {
     return await account.create(ID.unique(), email, password);
   } catch (error: any) {
     console.error(error);
+    throw error;
   }
 }
 
-
-export async function createWeeklyPicks(data:{gameId: string, gameWeekId: string, userResults: string}) {
-    try {
-        return await databases.updateDocument(appwriteConfig.databaseId, "66313025000612a5380e", "663130a100297f77c3c8", data);
-    } catch (err) {
-        console.error(err);
-    }
+export async function createWeeklyPicks(data: {
+  gameId: string;
+  gameWeekId: string;
+  userResults: string;
+}) {
+  try {
+    return await databases.updateDocument(
+      appwriteConfig.databaseId,
+      '66313025000612a5380e',
+      '663130a100297f77c3c8',
+      data,
+    );
+  } catch (err) {
+    console.error(err);
+  }
 }
---
+++ app/register/page.tsx
@@ -0,0 +1,115 @@
+'use client';
+import { useState, ChangeEvent, useEffect } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
+import { Input } from '@/components/Input/Input';
+import Logo from '@/components/Logo/Logo';
+import { Button } from '@/components/Button/Button';
+import { loginAccount, registerAccount } from '@/api/apiFunctions';
+
+import logo from '/public/assets/logo-colored-outline.svg';
+import { Models } from 'appwrite';
+
+export default function () {
+  const router = useRouter();
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [confirmPassword, setConfirmPassword] = useState<string>('');
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+  const handleEmail = (event: ChangeEvent<HTMLInputElement>): void => {
+    setEmail(event.target.value);
+  };
+
+  const handlePassword = (event: ChangeEvent<HTMLInputElement>): void => {
+    setPassword(event.target.value);
+  };
+
+  const comparePasswords = (password: string, confirmPassword: string) => {
+    return password === confirmPassword;
+  };
+
+  const handleConfirmPassword = (
+    event: ChangeEvent<HTMLInputElement>,
+  ): void => {
+    setConfirmPassword(event.target.value);
+  };
+
+  const handleDisabled = () => {
+    const passwordsMatch = comparePasswords(password, confirmPassword);
+    if (email && passwordsMatch === true && confirmPassword != '') {
+      return false;
+    }
+    return true;
+  };
+
+  const handleRegister = async () => {
+    try {
+      await registerAccount({ email, password });
+    } catch (error) {
+      console.error(error);
+    } finally {
+      loginAccount({ email, password });
+      if (isLoggedIn) {
+        router.push('/weeklyPicks');
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      router.push('/weeklyPicks');
+    }
+  }, []);
+
+  return (
+    <div className="h-screen w-full">
+      <div className="grid h-screen w-full grid-cols-2 bg-gradient-to-b from-[#4E160E] to-zinc-950">
+        <div className="grid p-8">
+          <div className="grid">
+            <Logo className="mx-auto place-self-end" src={logo} />
+          </div>
+          <div className="mx-auto grid gap-4 place-self-end">
+            <p className="leading-7 text-white">
+              Thank you... fantasy football draft, for letting me know that even
+              in my fantasies, I am bad at sports.
+            </p>
+            <p className="leading-7 text-white">Jimmy Fallon</p>
+          </div>
+        </div>
+        <div className="grid place-content-center bg-white p-8">
+          <div className="mx-auto grid w-80 place-content-center gap-4">
+            <h1 className="text-5xl font-extrabold tracking-tight text-foreground">
+              Register a new account
+            </h1>
+
+            <Input
+              type="email"
+              value={email}
+              placeholder="Email"
+              onChange={handleEmail}
+            />
+            <Input
+              type="password"
+              value={password}
+              placeholder="Password"
+              onChange={handlePassword}
+            />
+            <Input
+              type="password"
+              value={confirmPassword}
+              placeholder="Confirm Password"
+              onChange={handleConfirmPassword}
+            />
+            <Button
+              label="Register"
+              disabled={handleDisabled()}
+              onClick={handleRegister}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
