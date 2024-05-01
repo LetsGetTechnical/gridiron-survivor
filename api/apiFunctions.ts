673bdd429edb3f3adc5bd544a1fde2a0023a02e8
Carlos <54788134+Clue355@users.noreply.github.com>
2024-05-01 16:49:49 -0500

    Registration page (#173)
    - added registration logic
    - checks if user is logged in and redirects
    
    ---------
    
    Co-authored-by: Ryan Furrer <ryandotfurrer@gmail.com>
    Co-authored-by: Mai Vang <vmaineng@gmail.com>

--- api/apiFunctions.ts
+++ api/apiFunctions.ts
@@ -16,13 +16,19 @@ export async function logoutAccount() {
   }
 }
 
-export async function getUserWeeklyPick(data: {userId: string, weekNumber: string}) {
-    try {
-        const response = await databases.listDocuments(appwriteConfig.databaseId, "66313025000612a5380e");
-        return JSON.parse(response.documents[0].userResults)[data.userId];
-    } catch (err) {
-        console.error(err);
-    }
+export async function getUserWeeklyPick(data: {
+  userId: string;
+  weekNumber: string;
+}) {
+  try {
+    const response = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      '66313025000612a5380e',
+    );
+    return JSON.parse(response.documents[0].userResults)[data.userId];
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 export async function registerAccount(request: {
@@ -34,15 +40,23 @@ export async function registerAccount(request: {
   try {
     return await account.create(ID.unique(), email, password);
   } catch (error: any) {
-    console.error(error);
+    throw error;
   }
 }
 
-
-export async function createWeeklyPicks(data:{gameId: string, gameWeekId: string, userResults: string}) {
-    try {
-        return await databases.updateDocument(appwriteConfig.databaseId, "66313025000612a5380e", "663130a100297f77c3c8", data);
-    } catch (err) {
-        console.error(err);
-    }
+export async function createWeeklyPicks(data: {
+  gameId: string;
+  gameWeekId: string;
+  userResults: string;
+}) {
+  try {
+    return await databases.updateDocument(
+      appwriteConfig.databaseId,
+      '66313025000612a5380e',
+      '663130a100297f77c3c8',
+      data,
+    );
+  } catch (err) {
+    console.error(err);
+  }
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/login',
+          permanent: true,
+        },
+      ]
+    },
+  }
