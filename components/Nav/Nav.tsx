29b5b7f28c483e7a8cc4e38416e947acb8a35a0e
Mai Vang <vmaineng@gmail.com>
2024-05-01 21:13:52 -0700

    removed console.log

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -19,7 +19,6 @@ export const Nav = () => {
   const router = useRouter();
   const pathname = usePathname();
 
-  console.log(pathname);
   const handleLogout = async () => {
     try {
       await logoutAccount();
