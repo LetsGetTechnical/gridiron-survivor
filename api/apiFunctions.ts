a9c159d094add33fd2173d615f96671a5706d6eb
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2024-05-07 15:34:22 -0400

    quick fix of imports from apiFunctions.ts

--- api/apiFunctions.ts
+++ api/apiFunctions.ts
@@ -1,20 +1,20 @@
 import { account, databases, ID, appwriteConfig } from './config';
 
-// export async function loginAccount(user: { email: string; password: string }) {
-//   try {
-//     return await account.createEmailPasswordSession(user.email, user.password);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
+export async function loginAccount(user: { email: string; password: string }) {
+  try {
+    return await account.createEmailPasswordSession(user.email, user.password);
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-// export async function logoutAccount() {
-//   try {
-//     return await account.deleteSession('current');
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
+export async function logoutAccount() {
+  try {
+    return await account.deleteSession('current');
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 export async function getUserWeeklyPick(data: {
   userId: string;
