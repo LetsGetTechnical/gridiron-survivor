a88450c382dc67977010a1dfabc9f5bc134113cd
choir27 <richardchoi54@gmail.com>
2024-05-08 15:57:35 -0400

    add CI CD Github Actions, Docker, Game Data API, Github, MVVM, Next Routing, Performance, Playwright, Playwright for E2E, pnpm, prettier, react testing, next, responsive, session storage, shadcn, storybook, supabase, twilio email and text, typescript, vercel, webpack, and zustand

--- next.config.js
+++ next.config.js
@@ -1,11 +1,22 @@
-module.exports = {
+const images = require('remark-images')
+const emoji = require('remark-emoji')
+
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [images, emoji]
+  }
+})
+
+module.exports =  withMDX({
     async redirects() {
       return [
         {
+          pageExtensions: ['js', 'jsx', 'md', 'mdx'],
           source: '/',
           destination: '/login',
           permanent: true,
         },
       ]
     },
-  }
+  });
