f19c63b4c94a88b5fb9dd9f866ddf7dd270167d9
choir27 <richardchoi54@gmail.com>
2024-05-10 18:43:26 -0400

    convert all research documentation into mdx syntax

--- next.config.js
+++ next.config.js
@@ -1,22 +1,11 @@
-const images = require('remark-images')
-const emoji = require('remark-emoji')
-
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [images, emoji]
-  }
-})
-
-module.exports =  withMDX({
-    async redirects() {
-      return [
-        {
-          pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-          source: '/',
-          destination: '/login',
-          permanent: true,
-        },
-      ]
-    },
-  });
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: true,
+      },
+    ]
+  },
+}
