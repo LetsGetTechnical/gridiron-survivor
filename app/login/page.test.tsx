9983b46c33ca6eae0588942c9f774eb4e3776368
Ryan Furrer <ryandotfurrer@gmail.com>
2024-05-10 15:18:48 -0400

    Add current attempt of unit test for the login functionality.

--- app/login/page.test.tsx
+++ app/login/page.test.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import { Input } from '@/components/Input/Input';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Logo from '@/components/Logo/Logo';
+import { Button } from '@/components/Button/Button';
+import Login from './page';
 
 describe('Login Page', () => {
   it('Checks that the Logo image rendered has the correct test-id and src', () => {
@@ -23,5 +25,26 @@ describe('Login Page', () => {
     const passwordInput = screen.getByPlaceholderText('Password');
     expect(passwordInput).toBeInTheDocument();
     expect(passwordInput).toHaveAttribute('type', 'password');
+  });
+  it('Mocks the login process', () => {
+    jest.mock('./page.tsx', () => ({
+      Login: jest.fn().mockReturnValue({ email: 'testemail@email.com' }),
+    }));
+    // Render the input and button components
+    render(<Input type="email" placeholder="Email" />);
+    const emailInput = screen.getByPlaceholderText('Email');
+    render(<Input type="password" placeholder="Password" />);
+    const passwordInput = screen.getByPlaceholderText('Password');
+    render(<button role="button">Continue</button>);
+
+    // Find the button by it's role
+    const button = screen.getByRole('button');
+
+    // Mock the click event
+    fireEvent.click(button);
+
+    // Checke that the Login function was called
+    expect(Login()).toHaveBeenCalled();
+    expect(Login()).toEqual({ email: 'testemail@email.com' });
   });
 });
