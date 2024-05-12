84962f7121c15f076ff0bfe87fb084ffdaf97335
Mai Vang <vmaineng@gmail.com>
2024-05-11 18:36:10 -0700

    reverted NavDrawer

--- components/NavDrawer/NavDrawer.tsx
+++ components/NavDrawer/NavDrawer.tsx
@@ -1,7 +1,9 @@
 'use client';
+
 import * as React from 'react';
 import { Drawer as DrawerPrimitive } from 'vaul';
 import { X } from 'lucide-react';
+
 import { cn } from '@/lib/utils';
 
 const Drawer = ({
