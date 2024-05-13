e7c04253df4874ac3f3c2a80231aec8e784554cd
Mai Vang <vmaineng@gmail.com>
2024-05-13 12:14:05 -0700

    wrote unit tests for NFLteams, weeklypicks, user weeklypicks

--- store/dataStore.test.ts
+++ store/dataStore.test.ts
@@ -4,6 +4,12 @@ import { useDataStore } from './dataStore';
 // test values
 const userId = '123';
 const userEmail = 'test@email.com';
+const teamName = "Philadelphia Eagles";
+const teamLogo = "eagle";
+const weekNumber = '7';
+const gameId = "438";
+const gameWeekId = "7839";
+const userResults = "Vikings";
 
 describe('Data Store', () => {
   it('Check the default user state', () => {
@@ -31,5 +37,52 @@ describe('Data Store', () => {
 
     expect(result.current.user.id).toBe('');
     expect(result.current.user.email).toBe('');
+  });
+  it('Check the default NFL Teams state', () => {
+    const { result } = renderHook(() => useDataStore());
+    expect(result.current.NFLTeams.teamName).toBe('');
+    expect(result.current.NFLTeams.teamLogo).toBe('');
+  });
+  it('Check the updated NFL Teams state', () => {
+    const { result } = renderHook(() => useDataStore());
+
+    act(() => {
+      result.current.updateNFLTeams(teamName, teamLogo);
+    });
+
+    expect(result.current.NFLTeams.teamName).toBe(teamName);
+    expect(result.current.NFLTeams.teamLogo).toBe(teamLogo);
+  });
+  it('Check the default userWeeklyPick state', () => {
+    const { result } = renderHook(() => useDataStore());
+    expect(result.current.user.id).toBe('');
+    expect(result.current.userWeeklyPick.weekNumber).toBe('');
+  });
+  it('Check the updated userWeeklyPick state', () => {
+    const { result } = renderHook(() => useDataStore());
+
+    act(() => {
+      result.current.updateUserWeeklyPick(userId, weekNumber);
+    });
+
+    expect(result.current.user.id).toBe(userId);
+    expect(result.current.userWeeklyPick.weekNumber).toBe(weekNumber);
+  });
+  it('Check the default updateWeeklyPicks state', () => {
+    const { result } = renderHook(() => useDataStore());
+    expect(result.current.weeklyPicks.gameId).toBe('');
+    expect(result.current.weeklyPicks.gameWeekId).toBe('');
+    expect(result.current.weeklyPicks.userResults).toBe('');
+  });
+  it('Check the updated updateWeeklyPicks state', () => {
+    const { result } = renderHook(() => useDataStore());
+
+    act(() => {
+      result.current.updateWeeklyPicks(gameId, gameWeekId, userResults);
+    });
+
+    expect(result.current.weeklyPicks.gameId).toBe(gameId);
+    expect(result.current.weeklyPicks.gameWeekId).toBe(gameWeekId);
+    expect(result.current.weeklyPicks.userResults).toBe(userResults);
   });
 });
