fc29ba4e83cfce92f845d5be13c5655f277d8f57
Mai Vang <vmaineng@gmail.com>
2024-05-16 12:25:39 -0700

    pulled in from develop

--- jest.config.ts
+++ jest.config.ts
@@ -13,6 +13,7 @@ const config: Config = {
   // Add more setup options before each test is run
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   preset: "ts-jest",
+  testMatch: ['**/*.test.ts', '**/*.test.tsx'],
   testPathIgnorePatterns: [
     "<rootDir>/tests/playwright-tests/",
   ]
