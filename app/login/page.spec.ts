a22eb63d01b67728f41285ab5c1b0af9a0c44653
Ryan Furrer <ryandotfurrer@gmail.com>
2024-05-16 18:50:27 -0400

    Revise /login playwright test to remove console logs and test when auth fails

--- app/login/page.spec.ts
+++ app/login/page.spec.ts
@@ -1,23 +1,19 @@
 import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
+test('should successfully login', async ({ page }) => {
   await page.goto('/login');
-
   await page.getByTestId('email').click();
   await page.getByTestId('email').fill('testemail@email.com');
-
   await page.getByTestId('password').click();
   await page.getByTestId('password').fill('test1234');
-
   await page.getByTestId('continue-button').click();
-
   await expect(page).toHaveURL('/weeklyPicks');
-
-  const urlAfterLogin = await page.url();
-  console.log(urlAfterLogin);
-  if (urlAfterLogin === '/weeklyPicks') {
-    console.log('Successful login');
-  } else {
-    console.log('Failed to login');
-  }
+});
+test('should fail login', async ({ page }) => {
+  await page.goto('/login');
+  await page.getByTestId('email').click();
+  await page.getByTestId('email').fill('wrongemail@email.com');
+  await page.getByTestId('password').click();
+  await page.getByTestId('password').fill('wrongpassword');
+  await page.getByTestId('continue-button').click();
+  await expect(page).toHaveURL('/login');
 });
