37b1b7c97e874bc1696162f74a00b5752f013265
Ryan Furrer <ryandotfurrer@gmail.com>
2024-05-17 16:23:52 -0400

    Comment out current Playwright Testing job in github action and create new one

--- playwright.config.ts
+++ playwright.config.ts
@@ -43,10 +43,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL:
-      currentBranch === 'main'
-        ? 'https://www.gridironsurvivor.com'
-        : 'http://localhost:3000',
+    baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -73,10 +70,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: currentBranch === 'main' ? 'npm run build' : 'npm run dev',
-    url:
-      currentBranch === 'main'
-        ? 'https://www.gridironsurvivor.com'
-        : 'http://localhost:3000',
+    url: process.env.PLAYWRIGHT_TEST_BASE_URL || 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
   },
 });
