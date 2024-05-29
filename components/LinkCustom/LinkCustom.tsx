d4bb9b975928a40705ffe01f9e45edd7b4686e81
Ryan Furrer <ryandotfurrer@gmail.com>
2024-05-29 18:30:26 -0400

    Add visually-hidden span to LinkCustom component for screen reader users

--- components/LinkCustom/LinkCustom.tsx
+++ components/LinkCustom/LinkCustom.tsx
@@ -1,19 +1,23 @@
 // Import necessary modules from Next.js and React
 import Link from 'next/link';
-import React, { ReactElement } from 'react';
+import React from 'react';
 
 interface ILinkCustomProps {
   href: string;
   children: string;
   className?: string;
   newTab?: boolean;
+  visuallyHiddenText?: string;
 }
+
+// Be sure to add visuallyHiddenText to your link to add additional context for screen reader users. Read more at https://webforeveryone.us/blog/fixing-the-six-most-common-bugs#links-with-discernable-text
 
 const LinkCustom = ({
   href,
   children,
   className,
   newTab = false,
+  visuallyHiddenText,
 }: ILinkCustomProps) => {
   // Determine if the target attribute should be added based on the newTab prop
   const targetAttr = newTab ? '_blank' : '_self';
@@ -28,6 +32,7 @@ const LinkCustom = ({
       passHref
       className={`hover:text-orange-600 hover:underline ${className}`}
     >
+      <span className="sr-only">{visuallyHiddenText}</span>
       {children}
     </Link>
   );
