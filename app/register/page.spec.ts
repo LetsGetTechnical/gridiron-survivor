c32ad15f1a332da109e5388f0916d198a379d728
Mai Vang <vmaineng@gmail.com>
2024-05-29 15:46:43 -0700

    added in json

--- app/register/page.spec.ts
+++ app/register/page.spec.ts
@@ -16,14 +16,13 @@ test.beforeEach(async ({ page }) => {
   await page.goto('/register');
 });
 
-//happy path
 test.describe('Tests register page', () => {
   test('should successfully register', async ({ page }) => {
     await page.getByTestId('email').fill(goodUser.email);
     await page.getByTestId('password').fill(goodUser.password);
     await page.getByTestId('confirm-password').fill(goodUser.confirmPassword);
     await page.getByTestId('continue-button').click();
-    await page.waitForURL('/weeklyPicks', { timeout: 500000 });
+    // await page.waitForURL('/weeklyPicks', { timeout: 500000 });
     await expect(page).toHaveURL('/weeklyPicks');
   });
   test('should not be able to register and register button should be disabled', async ({
