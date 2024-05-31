346ad3f601d7889673d1f69ce3eac9e0d6096afc
Mai Vang <vmaineng@gmail.com>
2024-05-31 16:19:31 -0700

    modify test

--- app/register/page.spec.ts
+++ app/register/page.spec.ts
@@ -1,7 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-//need different users for different browser tests
-
 const goodUser = {
   email: 'test1@email.com',
   password: 'test12345',
@@ -24,10 +22,8 @@ test.describe('Tests register page', () => {
     await page.getByTestId('password').fill(goodUser.password);
     await page.getByTestId('confirm-password').fill(goodUser.confirmPassword);
     await page.getByTestId('continue-button').click();
-    await page.waitForURL('/weeklyPicks');
-    expect (page.url()).toBe('/weeklyPicks');
-    // await expect(page).not.toHaveURL('/register');
-    //  await expect(page).toHaveURL('/weeklyPicks');
+    await page.goto('http://localhost:3000/weeklyPicks');
+    await expect(page.url()).toBe('http://localhost:3000/weeklyPicks');
   });
   test('should not be able to register and register button should be disabled', async ({
     page,
