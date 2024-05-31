9f276213e97cbdb5c78fd315f004c30cf38b93aa
choir <richardchoi54@gmail.com>
2024-05-31 13:16:49 -0400

    ops: #216 fix error on build for error boundary

--- app/error.tsx
+++ app/error.tsx
@@ -1,6 +1,10 @@
 'use client'; // Error components must be Client Components
 
-export default function ErrorBoundary() {
+interface IErrorBoundary{
+  children? : React.ReactNode
+}
+
+export default function ErrorBoundary(props: IErrorBoundary) {
   return (
     <div className="align-center flex flex-col">
       <h2 className="text-white">Something went wrong!</h2>
