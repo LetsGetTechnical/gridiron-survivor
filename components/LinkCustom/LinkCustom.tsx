8959f86b5a3b89cb96fc8b1da95b654248957bf4
Ryan Furrer <ryandotfurrer@gmail.com>
2024-06-01 09:19:36 -0400

    fix(linkCustom) REQUIRED: Removed unused props from and alphebetized props where defined

--- components/LinkCustom/LinkCustom.tsx
+++ components/LinkCustom/LinkCustom.tsx
@@ -3,34 +3,25 @@ import Link from 'next/link';
 import React from 'react';
 
 interface ILinkCustomProps {
-  href: string;
   children: string;
-  className?: string;
-  newTab?: boolean;
+  href: string;
   visuallyHiddenText?: string;
 }
 
 // Be sure to add visuallyHiddenText to your link to add additional context for screen reader users. Read more at https://webforeveryone.us/blog/fixing-the-six-most-common-bugs#links-with-discernable-text
 
 const LinkCustom = ({
-  href,
   children,
-  className,
-  newTab = false,
+  href,
   visuallyHiddenText,
 }: ILinkCustomProps) => {
-  // Determine if the target attribute should be added based on the newTab prop
-  const targetAttr = newTab ? '_blank' : '_self';
-
   // Use Next.js Link component wrapped inside an anchor tag
   return (
     <Link
       data-testid="linkCustom"
-      target={targetAttr}
-      rel={newTab ? 'noopener noreferrer' : undefined}
       href={href}
       passHref
-      className={`hover:text-orange-600 hover:underline ${className}`}
+      className={'hover:text-orange-600 hover:underline'}
     >
       <span className="sr-only">{visuallyHiddenText}</span>
       {children}
