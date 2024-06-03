8c481a76661f7089be9b02054a1b889804505184
Ryan Furrer <ryandotfurrer@gmail.com>
2024-06-03 09:27:09 -0400

    feat(LinkCustom): removed unnecessary testing from LinkCustom.test.tsx file

--- components/LinkCustom/LinkCustom.test.tsx
+++ components/LinkCustom/LinkCustom.test.tsx
@@ -3,35 +3,12 @@ import { render, screen } from '@testing-library/react';
 import LinkCustom from './LinkCustom';
 
 describe('LinkCustom Component', () => {
-  it('renders with default props and does not open in a new tab', () => {
+  it('renders with default props', () => {
     render(
-      <LinkCustom
-        children="Test link that should open in the same tab when clicked"
-        href="https://example.com"
-      ></LinkCustom>,
+      <LinkCustom children="Test link" href="https://example.com"></LinkCustom>,
     );
     const link = screen.getByTestId('linkCustom');
     expect(link).toBeInTheDocument();
-    expect(link).toHaveTextContent(
-      'Test link that should open in the same tab when clicked',
-    );
-    expect(link).not.toHaveAttribute('target', '_blank');
-  });
-
-  it('opens in a new tab when newTab is true', () => {
-    render(
-      <LinkCustom
-        children="Test link that should open in a new tab when clicked"
-        href="https://example.com"
-        newTab
-      ></LinkCustom>,
-    );
-    const link = screen.getByTestId('linkCustom');
-    expect(link).toBeInTheDocument();
-    expect(link).toHaveTextContent(
-      'Test link that should open in a new tab when clicked',
-    );
-    expect(link).toHaveAttribute('target', '_blank');
-    expect(link).toHaveAttribute('rel', 'noopener noreferrer');
+    expect(link).toHaveTextContent('Test link');
   });
 });
