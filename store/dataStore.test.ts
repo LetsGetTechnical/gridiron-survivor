b6ec65522633e9930e8ed40508487fbf0cfdecef
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2024-06-08 14:24:48 -0400

    refactor(dataStore.test.ts): grouped related tests

--- store/dataStore.test.ts
+++ store/dataStore.test.ts
@@ -33,116 +33,133 @@ const gameCurrentWeek = {
 };
 
 describe('Data Store', () => {
-  it('Check the default user state', () => {
-    const { result } = renderHook(() => useDataStore());
-    expect(result.current.user.id).toBe('');
-    expect(result.current.user.email).toBe('');
-    expect(result.current.user.league).toStrictEqual([]);
-  });
-  it('Check the updated user state', () => {
-    const { result } = renderHook(() => useDataStore());
-
-    act(() => {
-      result.current.updateUser(
-        userData.userId,
-        userData.userEmail,
-        userData.league,
-      );
+  describe('User Test', () => {
+    it('Check the default user state', () => {
+      const { result } = renderHook(() => useDataStore());
+      expect(result.current.user.id).toBe('');
+      expect(result.current.user.email).toBe('');
+      expect(result.current.user.league).toStrictEqual([]);
     });
+    it('Check the updated user state', () => {
+      const { result } = renderHook(() => useDataStore());
 
-    expect(result.current.user.id).toBe(userData.userId);
-    expect(result.current.user.email).toBe(userData.userEmail);
-  });
-  it('Checks the reset user state matches default', () => {
-    const { result } = renderHook(() => useDataStore());
+      act(() => {
+        result.current.updateUser(
+          userData.userId,
+          userData.userEmail,
+          userData.league,
+        );
+      });
 
-    act(() => {
-      result.current.updateUser(
-        userData.userId,
-        userData.userEmail,
-        userData.league,
-      );
-      result.current.resetUser();
+      expect(result.current.user.id).toBe(userData.userId);
+      expect(result.current.user.email).toBe(userData.userEmail);
     });
+    it('Checks the reset user state matches default', () => {
+      const { result } = renderHook(() => useDataStore());
 
-    expect(result.current.user.id).toBe('');
-    expect(result.current.user.email).toBe('');
-  });
-  it('Check the default NFL Teams state', () => {
-    const { result } = renderHook(() => useDataStore());
-    expect(result.current.NFLTeam).toStrictEqual([]);
-  });
-  it('Check the updated NFL Teams state', () => {
-    const { result } = renderHook(() => useDataStore());
+      act(() => {
+        result.current.updateUser(
+          userData.userId,
+          userData.userEmail,
+          userData.league,
+        );
+        result.current.resetUser();
+      });
 
-    act(() => {
-      result.current.updateNFLTeam(NFLTeam);
+      expect(result.current.user.id).toBe('');
+      expect(result.current.user.email).toBe('');
     });
+  });
 
-    expect(result.current.NFLTeam[0]).toBe(NFLTeam[0]);
-    expect(result.current.NFLTeam[1]).toBe(NFLTeam[1]);
+  describe('NFL Teams Test', () => {
+    it('Check the default NFL Teams state', () => {
+      const { result } = renderHook(() => useDataStore());
+      expect(result.current.NFLTeam).toStrictEqual([]);
+    });
+    it('Check the updated NFL Teams state', () => {
+      const { result } = renderHook(() => useDataStore());
+
+      act(() => {
+        result.current.updateNFLTeam(NFLTeam);
+      });
+
+      expect(result.current.NFLTeam[0]).toBe(NFLTeam[0]);
+      expect(result.current.NFLTeam[1]).toBe(NFLTeam[1]);
+    });
   });
-  it('Check the default weeklyPicks state', () => {
-    const { result } = renderHook(() => useDataStore());
-    expect(result.current.weeklyPicks.leagueId).toBe('');
-    expect(result.current.weeklyPicks.gameWeekId).toBe('');
-    expect(result.current.weeklyPicks.userResults).toStrictEqual({});
-  });
-  it('Check the updated weeklyPick state', () => {
-    const { result } = renderHook(() => useDataStore());
-    const weekData = {
-      leagueId: '123',
-      gameWeekId: '456',
-      userResults: {
-        '66281d5ec5614f76bc91': {
-          team: '66218f22b40deef340f8',
-          correct: false,
+
+  describe('Weekly Picks Test', () => {
+    it('Check the default weeklyPicks state', () => {
+      const { result } = renderHook(() => useDataStore());
+      expect(result.current.weeklyPicks.leagueId).toBe('');
+      expect(result.current.weeklyPicks.gameWeekId).toBe('');
+      expect(result.current.weeklyPicks.userResults).toStrictEqual({});
+    });
+    it('Check the updated weeklyPick state', () => {
+      const { result } = renderHook(() => useDataStore());
+      const weekData = {
+        leagueId: '123',
+        gameWeekId: '456',
+        userResults: {
+          '66281d5ec5614f76bc91': {
+            team: '66218f22b40deef340f8',
+            correct: false,
+          },
+          '6628077faeeedd272637': {
+            team: '6621b30ea57bd075e9d3',
+            correct: false,
+          },
+          '66174f2362ec891167be': {
+            team: '6621b30ea57bd075e9d3',
+            correct: true,
+          },
         },
-        '6628077faeeedd272637': {
-          team: '6621b30ea57bd075e9d3',
-          correct: false,
-        },
-        '66174f2362ec891167be': { team: '6621b30ea57bd075e9d3', correct: true },
-      },
-    };
-    act(() => {
-      result.current.updateWeeklyPicks(weekData);
+      };
+      act(() => {
+        result.current.updateWeeklyPicks(weekData);
+      });
+
+      expect(result.current.weeklyPicks.leagueId).toBe(weekData.leagueId);
+      expect(result.current.weeklyPicks.gameWeekId).toBe(weekData.gameWeekId);
+      expect(result.current.weeklyPicks.userResults).toBe(weekData.userResults);
     });
-
-    expect(result.current.weeklyPicks.leagueId).toBe(weekData.leagueId);
-    expect(result.current.weeklyPicks.gameWeekId).toBe(weekData.gameWeekId);
-    expect(result.current.weeklyPicks.userResults).toBe(weekData.userResults);
   });
-  it('Check the default league state', () => {
-    const { result } = renderHook(() => useDataStore());
-    expect(result.current.league.leagueId).toBe('');
-    expect(result.current.league.participants).toStrictEqual([]);
-    expect(result.current.league.survivors).toStrictEqual([]);
-  });
-  it('Check the updated league state', () => {
-    const { result } = renderHook(() => useDataStore());
 
-    act(() => {
-      result.current.updateLeague(league);
+  describe('League Test', () => {
+    it('Check the default league state', () => {
+      const { result } = renderHook(() => useDataStore());
+      expect(result.current.league.leagueId).toBe('');
+      expect(result.current.league.participants).toStrictEqual([]);
+      expect(result.current.league.survivors).toStrictEqual([]);
     });
+    it('Check the updated league state', () => {
+      const { result } = renderHook(() => useDataStore());
 
-    expect(result.current.league.leagueId).toBe(league.leagueId);
-    expect(result.current.league.participants).toBe(league.participants);
-    expect(result.current.league.survivors).toBe(league.survivors);
-  });
-  it('Check the default gameCurrentWeek state', () => {
-    const { result } = renderHook(() => useDataStore());
-    expect(result.current.gameWeek.id).toBe('');
-    expect(result.current.gameWeek.week).toBe(0);
-  });
-  it('Check the updated gameCurrentWeek state', () => {
-    const { result } = renderHook(() => useDataStore());
+      act(() => {
+        result.current.updateLeague(league);
+      });
 
-    act(() => {
-      result.current.updateGameWeek(gameCurrentWeek);
+      expect(result.current.league.leagueId).toBe(league.leagueId);
+      expect(result.current.league.participants).toBe(league.participants);
+      expect(result.current.league.survivors).toBe(league.survivors);
     });
+  });
 
-    expect(result.current.gameWeek.id).toBe(gameCurrentWeek.id);
-    expect(result.current.gameWeek.week).toBe(gameCurrentWeek.week);
+  describe('Game Current Week Test', () => {
+    it('Check the default gameCurrentWeek state', () => {
+      const { result } = renderHook(() => useDataStore());
+      expect(result.current.gameWeek.id).toBe('');
+      expect(result.current.gameWeek.week).toBe(0);
+    });
+    it('Check the updated gameCurrentWeek state', () => {
+      const { result } = renderHook(() => useDataStore());
+
+      act(() => {
+        result.current.updateGameWeek(gameCurrentWeek);
+      });
+
+      expect(result.current.gameWeek.id).toBe(gameCurrentWeek.id);
+      expect(result.current.gameWeek.week).toBe(gameCurrentWeek.week);
+    });
   });
 });
