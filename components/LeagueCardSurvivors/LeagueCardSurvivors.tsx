7b4d13a34f0494b61b058d006dbeb1e9ea56f0b1
Mai Vang <vmaineng@gmail.com>
2024-06-08 15:24:55 -0700

    unblocked tests

--- components/LeagueCardSurvivors/LeagueCardSurvivors.tsx
+++ components/LeagueCardSurvivors/LeagueCardSurvivors.tsx
@@ -1,31 +1,31 @@
-// import { cn } from '@/utils/utils';
-// import { ILeagueCardSurvivorsProps } from './ILeagueCardSurvivorsProps';
-// import * as React from 'react';
+import { cn } from '@/utils/utils';
+import { ILeagueCardSurvivorsProps } from './ILeagueCardSurvivorsProps';
+import * as React from 'react';
 
-// const LeagueCardSurvivors = React.forwardRef<
-//   HTMLParagraphElement,
-//   ILeagueCardSurvivorsProps
-// >(({ isEliminated, survivors, totalPlayers, ...props }) => (
-//   <p
-//     data-testid="LeagueCardSurvivors"
-//     className={cn(
-//       'LeagueCardSurvivors text-sm text-foreground',
-//       isEliminated ? 'text-foreground/50' : '',
-//     )}
-//     {...props}
-//   >
-//     {isEliminated ? 'ELIMINATED' : `Survivors ${survivors} `}
-//     {isEliminated === false && (
-//       <span
-//         data-testid="LeagueCardSurvivorsTotalPlayers"
-//         className="text-foreground/50"
-//       >
-//         {' '}
-//         / {totalPlayers}
-//       </span>
-//     )}
-//   </p>
-// ));
-// LeagueCardSurvivors.displayName = 'LeagueCardSurvivors';
+const LeagueCardSurvivors = React.forwardRef<
+  HTMLParagraphElement,
+  ILeagueCardSurvivorsProps
+>(({ isEliminated, survivors, totalPlayers, ...props }) => (
+  <p
+    data-testid="LeagueCardSurvivors"
+    className={cn(
+      'LeagueCardSurvivors text-sm text-foreground',
+      isEliminated ? 'text-foreground/50' : '',
+    )}
+    {...props}
+  >
+    {isEliminated ? 'ELIMINATED' : `Survivors ${survivors} `}
+    {isEliminated === false && (
+      <span
+        data-testid="LeagueCardSurvivorsTotalPlayers"
+        className="text-foreground/50"
+      >
+        {' '}
+        / {totalPlayers}
+      </span>
+    )}
+  </p>
+));
+LeagueCardSurvivors.displayName = 'LeagueCardSurvivors';
 
-// export { LeagueCardSurvivors };
+export { LeagueCardSurvivors };
