56d04c08038b7493e96c0b0ef0657de0fd82acf3
Mai Vang <vmaineng@gmail.com>
2024-06-13 15:20:47 -0700

    added in js doc

--- components/Heading/Heading.tsx
+++ components/Heading/Heading.tsx
@@ -2,7 +2,14 @@
 // Licensed under the MIT License.
 
 import React from 'react';
-
+/**
+ * Renders a heading with the given level and text.
+ *
+ * @param as - The HTML element type to render.
+ * @param children - The text to display inside the heading.
+ * @param className - Additional CSS classes to apply to the heading.
+ * @return The rendered heading element.
+ */
 const Heading = React.forwardRef<
   React.ElementRef<'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'>,
   {
