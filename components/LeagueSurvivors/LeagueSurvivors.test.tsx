c061a51ec6599399385ffa31e24770bdeff165e0
Ryan Furrer <ryandotfurrer@gmail.com>
2024-06-15 12:00:30 -0400

    test(LeagueSurvivors): remove test checking eliminated state styling
    styling no longer needed as per design updates

--- components/LeagueSurvivors/LeagueSurvivors.test.tsx
+++ components/LeagueSurvivors/LeagueSurvivors.test.tsx
@@ -1,38 +1,14 @@
-import { LeagueCard } from '../LeagueCard/LeagueCard';
+import { LeagueSurvivors } from './LeagueSurvivors';
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 
 describe('LeagueSurvivors', () => {
   it('renders correctly if user is not eliminated', () => {
-    render(
-      <LeagueCard
-        href="#"
-        survivors={11}
-        title={'League 1'}
-        totalPlayers={12}
-      />,
-    );
+    render(<LeagueSurvivors survivors={11} totalPlayers={12} />);
 
     const leagueSurvivors = screen.getByTestId('LeagueSurvivors');
 
     expect(leagueSurvivors).toBeInTheDocument();
     expect(leagueSurvivors).toHaveTextContent('Survivors 11 / 12');
-  });
-  it('renders correctly if user is eliminated', () => {
-    render(
-      <LeagueCard
-        href="#"
-        isEliminated={true}
-        survivors={11}
-        title={'League 2'}
-        totalPlayers={12}
-      />,
-    );
-
-    const leagueSurvivors = screen.getByTestId('LeagueSurvivors');
-
-    expect(leagueSurvivors).toBeInTheDocument();
-    expect(leagueSurvivors).toHaveClass('text-foreground/50');
-    expect(leagueSurvivors).toHaveTextContent('ELIMINATED');
   });
 });
