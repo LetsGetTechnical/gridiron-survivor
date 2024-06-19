71abc6b2acfe4c0ba9c8aa534188a8e8ed5572cd
Ryan Furrer <ryandotfurrer@gmail.com>
2024-06-18 20:39:38 -0400

    fix(LeagueEntries.tsx): reduce imported react dependencies, simplified component function, correct custom css classes and data-testids, updated ternary logic, updated jsdocs
    imported only React from react rather than *, refactored component function so it is easier to see and changed it to a React.JSX.Elemtn rather than HTMLDivElement, made custom css classes and data-testids lowercase and hyphen separated rather than camelCase, removed unnecessary logic from ternary operators, updated JSDocs with missing information

--- components/LeagueEntries/LeagueEntries.tsx
+++ components/LeagueEntries/LeagueEntries.tsx
@@ -5,62 +5,71 @@ import { Button } from '../Button/Button';
 import { cn } from '@/utils/utils';
 import { EntryStatus } from '../EntryStatus/EntryStatus';
 import { ILeagueEntriesProps } from './LeagueEntries.interface';
-import * as React from 'react';
+import React from 'react';
 
-const LeagueEntries = React.forwardRef<HTMLDivElement, ILeagueEntriesProps>(
-  ({ entryNumber, isEliminated = false, isPickSet = false }, ref) => (
-    <div
-      data-testid="LeagueEntryContainerCard"
-      className={cn(
-        'LeagueEntryContainerCard grid h-20 min-w-fit grid-cols-2 justify-between rounded-lg border bg-card p-4 text-card-foreground shadow-sm outline dark:border-zinc-800',
-        isEliminated ? 'bg-zinc-100 dark:bg-zinc-900' : 'transparent',
-      )}
-      ref={ref}
+/**
+ * A card that contains information on the user's entry for this league. Contains the entry number, their entry status (alive or eliminated), team logo once a pick is set, and a button to make a pick or change their pick
+ * @param props - the props for LeagueEntries
+ * @param props.entryNumber - the entry number of the user in that specific league
+ * @param props.isEliminated - If true, the user is flagged as eliminat4ed
+ * @param props.isPickSet - if true, the team logo of the picked team shows up on the LeagueEntries card and the button changes from "make a pick" to "chagne pick"
+ * @returns {React.JSX.Element} - A div element that contains the user's entry information
+ */
+const LeagueEntries = ({
+  entryNumber,
+  isEliminated = false,
+  isPickSet = false,
+}: ILeagueEntriesProps): React.JSX.Element => (
+  <div
+    data-testid="league-entry-container-card"
+    className={cn(
+      'league-entry-container-card grid h-20 min-w-fit grid-cols-2 justify-between rounded-lg border bg-card p-4 text-card-foreground shadow-sm outline dark:border-zinc-800',
+      isEliminated ? 'bg-zinc-100 dark:bg-zinc-900' : 'transparent',
+    )}
+  >
+    <section
+      className="league-entry-header flex items-center gap-2 md:gap-12"
+      data-testid="league-entry-header"
     >
-      <section
-        className="LeagueEntryHeader flex items-center gap-2 md:gap-12"
-        data-testid="LeagueEntryHeader"
+      <h4
+        data-testid="league-entry-number"
+        className={cn(
+          'league-entry-number text-xl font-semibold leading-none tracking-tight',
+          isEliminated ? 'opacity-50' : '',
+        )}
       >
-        <h4
-          data-testid="LeagueEntryNumber"
-          className={cn(
-            'LeagueEntryNumber text-xl font-semibold leading-none tracking-tight',
-            isEliminated ? 'opacity-50' : '',
-          )}
-        >
-          Entry {entryNumber}
-        </h4>
-        <EntryStatus isEliminated={isEliminated} />
-      </section>
-      <section
-        className="LeagueEntryFooter flex items-center justify-end gap-2 md:gap-12"
-        data-testid="LeagueEntryFooter"
+        Entry {entryNumber}
+      </h4>
+      <EntryStatus isEliminated={isEliminated} />
+    </section>
+    <section
+      className="league-entry-footer flex items-center justify-end gap-2 md:gap-12"
+      data-testid="league-entry-footer"
+    >
+      {isPickSet && (
+        <img
+          className="league-entry-logo h-12 w-12"
+          data-testid="league-entry-logo"
+          src="https://ryanfurrer.com/_astro/logo-dark-theme.CS8e9u7V_JfowQ.svg"
+          alt="League name"
+        />
+      )}
+
+      <div
+        className="league-entry-pick-button-container"
+        data-testid="league-entry-pick-button-container"
       >
-        {isEliminated || !isPickSet ? null : (
-          <img
-            className="LeagueEntryLogo h-12 w-12"
-            data-testid="LeagueEntryLogo"
-            src="https://ryanfurrer.com/_astro/logo-dark-theme.CS8e9u7V_JfowQ.svg"
-            alt="League name"
+        {isEliminated ? null : (
+          <Button
+            className="league-entry-pick-button"
+            data-testid="league-entry-pick-button"
+            label={isPickSet ? 'Change Pick' : 'Make Pick'}
+            variant={isPickSet ? 'secondary' : 'default'}
           />
         )}
-
-        <div
-          className="LeagueEntryPickButtonContainer"
-          data-testid="LeagueEntryPickButtonContainer"
-        >
-          {isEliminated ? null : (
-            <Button
-              className="LeagueEntryPickButton"
-              data-testid="LeagueEntryPickButton"
-              label={isPickSet ? 'Change Pick' : 'Make Pick'}
-              variant={isPickSet ? 'secondary' : 'default'}
-            />
-          )}
-        </div>
-      </section>
-    </div>
-  ),
+      </div>
+    </section>
+  </div>
 );
 LeagueEntries.displayName = 'LeagueEntries';
 
