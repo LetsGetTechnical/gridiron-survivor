2fce06bfe08c24b0ffb31d762fad3b324727a6b4
Alex Appleget <alexappleget2014@gmail.com>
2024-06-24 12:54:35 -0500

    updated magicUrl route file

--- app/auth/magicUrl/route.ts
+++ app/auth/magicUrl/route.ts
@@ -10,12 +10,17 @@ import { NextResponse } from 'next/server';
  */
 export async function GET(request: Request): Promise<Response> {
   const requestUrl = new URL(request.url);
+  const targetUrl = new URL('/weeklyPicks', requestUrl.origin);
 
   const userId = requestUrl.searchParams.get('userId');
   const secret = requestUrl.searchParams.get('secret');
 
   if (!userId || !secret) {
     return new NextResponse('Invalid URL parameters', { status: 400 });
+  }
+
+  if (userId && secret) {
+    return NextResponse.redirect(targetUrl);
   }
 
   return NextResponse.redirect(requestUrl.origin);
