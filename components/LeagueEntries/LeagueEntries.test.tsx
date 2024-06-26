3a5aa2d97b7510dce17acbdd2fd1e3204f095867
Ryan Furrer <ryandotfurrer@gmail.com>
2024-06-25 20:59:21 -0400

    test(LeagueEntries.test.tsx): remove redundant lines from test

--- components/LeagueEntries/LeagueEntries.test.tsx
+++ components/LeagueEntries/LeagueEntries.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import React from 'react';
 
 describe('LeagueEntries', () => {
-  it('renders without a pick made', () => {
+  it(`renders 'default' state without a pick made`, () => {
     render(<LeagueEntries entryNumber={1} />);
 
     const leagueEntryContainerCard = screen.getByTestId(
@@ -13,16 +13,13 @@ describe('LeagueEntries', () => {
     const entryStatus = screen.getByTestId('entry-status');
     const leagueEntryPickButton = screen.getByTestId('league-entry-pick-button');
 
-    expect(entryStatus).toBeInTheDocument();
     expect(entryStatus).toHaveTextContent('alive');
     expect(leagueEntryContainerCard).toBeInTheDocument();
-    expect(leagueEntryNumber).toBeInTheDocument();
     expect(leagueEntryNumber).toHaveTextContent('Entry 1');
-    expect(leagueEntryPickButton).toBeInTheDocument();
     expect(leagueEntryPickButton).toHaveTextContent('Make Pick');
   });
 
-  it('renders with a pick made', () => {
+  it('renders as if the user made a pick', () => {
     render(<LeagueEntries entryNumber={2} isPickSet={true} />);
 
     const leagueEntryContainerCard = screen.getByTestId(
@@ -33,13 +30,9 @@ describe('LeagueEntries', () => {
     const leagueEntryLogo = screen.getByTestId('league-entry-logo');
     const leagueEntryPickButton = screen.getByTestId('league-entry-pick-button');
 
-    expect(entryStatus).toBeInTheDocument();
     expect(entryStatus).toHaveTextContent('alive');
     expect(leagueEntryContainerCard).toBeInTheDocument();
-    expect(leagueEntryLogo).toBeInTheDocument();
-    expect(leagueEntryNumber).toBeInTheDocument();
     expect(leagueEntryNumber).toHaveTextContent('Entry 2');
-    expect(leagueEntryPickButton).toBeInTheDocument();
     expect(leagueEntryPickButton).toHaveTextContent('Change Pick');
   });
 
@@ -52,13 +45,10 @@ describe('LeagueEntries', () => {
     const leagueEntryNumber = screen.getByTestId('league-entry-number');
     const entryStatus = screen.getByTestId('entry-status');
 
-    expect(entryStatus).toBeInTheDocument();
     expect(entryStatus).toHaveTextContent('eliminated');
-    expect(leagueEntryContainerCard).toBeInTheDocument();
     expect(leagueEntryContainerCard).toHaveClass(
       'bg-zinc-100 dark:bg-zinc-900',
     );
-    expect(leagueEntryNumber).toBeInTheDocument();
     expect(leagueEntryNumber).toHaveTextContent('Entry 3');
   });
 });
