d3474ae0e543cdba18e85d4de082b41e0542b13d
Clue355 <dm.carlosluevano@gmail.com>
2024-07-02 18:19:03 -0500

    test writen for alertnotifications

--- components/AlertNotification/AlertNotification.test.tsx
+++ components/AlertNotification/AlertNotification.test.tsx
@@ -1,8 +1,53 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import Alert from './AlertNotification';
 import { AlertVariants } from './Alerts.enum';
 import { CheckCircle, XCircle, Info, AlertTriangle } from 'lucide-react';
+
+import Nav from '../Nav/Nav';
+import Login from '@/app/login/page';
+
+const mockPush = jest.fn();
+const mockUsePathname = jest.fn();
+const mockLogoutAccount = jest.fn();
+
+const mockUseAuthContext = {
+  logoutAccount: mockLogoutAccount,
+  isSignedIn: true,
+};
+
+window.scrollTo = jest.fn();
+
+jest.mock('next/navigation', () => ({
+  useRouter() {
+    return {
+      push: mockPush,
+    };
+  },
+  usePathname() {
+    return mockUsePathname();
+  },
+}));
+
+jest.mock('../../context/AuthContextProvider', () => ({
+  useAuthContext() {
+    return {
+      ...mockUseAuthContext,
+    };
+  },
+}));
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
 
 const variantTestCases = {
   [AlertVariants.Success]: {
@@ -28,9 +73,37 @@ const variantTestCases = {
 };
 
 describe('AlertNotification', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   for (const [key, value] of Object.entries(variantTestCases)) {
     it(`renders the correct variant ${key}`, () => {
       render(<Alert variant={AlertVariants[key]} message={value.message} />);
     });
   }
+
+  it('should show up on login screen when a user signs out', async () => {
+    mockUsePathname.mockImplementation(() => '/weeklyPicks');
+
+    render(<Nav />);
+
+    const drawerTrigger = screen.getByTestId('drawer-trigger');
+    fireEvent.click(drawerTrigger);
+
+    const signOutButton = screen.getByTestId('sign-out-button');
+
+    fireEvent.click(signOutButton);
+    await waitFor(() => {
+      expect(mockLogoutAccount).toHaveBeenCalled();
+    });
+
+    mockUseAuthContext.isSignedIn = false;
+
+    render(<Login />);
+
+    const navElement = screen.getByTestId('nav');
+
+    expect(navElement).toBeInTheDocument();
+  });
 });
