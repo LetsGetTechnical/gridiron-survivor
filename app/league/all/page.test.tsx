eca56b2db2e57999a5c9ece07abeb12eea738302
Mai Vang <vmaineng@gmail.com>
2024-07-03 13:24:06 -0700

    updated test

--- app/league/all/page.test.tsx
+++ app/league/all/page.test.tsx
@@ -1,35 +1,38 @@
 import { render, screen, waitFor } from '@testing-library/react';
 import Leagues from './page';
+import { useDataStore } from '@/store/dataStore';
+import { getUserLeagues } from '@/utils/utils';
+import { getGameWeek } from '@/api/apiFunctions';
 
 jest.mock('@/store/dataStore', () => ({
-  useDataStore: jest.fn(),
+  useDataStore: jest.fn(() => ({ user: { id: '123', leagues: [] } })),
 }));
 
 jest.mock('@/utils/utils', () => ({
-  getUserLeagues: jest.fn(),
+  getUserLeagues: jest.fn(() => Promise.resolve([])),
 }));
 
 jest.mock('@/api/apiFunctions', () => ({
-  getGameWeek: jest.fn(),
+  getGameWeek: jest.fn(() =>
+    Promise.resolve({
+      week: 1,
+    }),
+  ),
 }));
 
 describe('Leagues Component', () => {
-  const mockUser = { id: '123', leagues: [] };
-  const mockUseDataStore = require('@/store/dataStore').useDataStore;
-  const mockGetUserLeagues = require('@/utils/utils').getUserLeagues;
-  const mockGetGameWeek = require('@/api/apiFunctions').getGameWeek;
+  const mockUseDataStore = useDataStore as jest.Mock;
+  const mockGetUserLeagues = getUserLeagues as jest.Mock;
+  const mockGetGameWeek = getGameWeek as jest.Mock;
 
   beforeEach(() => {
-    mockUseDataStore.mockReturnValue({ user: mockUser });
-    mockGetUserLeagues.mockResolvedValue([]);
-    mockGetGameWeek.mockResolvedValue({ week: 1 });
-  });
-
-  afterEach(() => {
     jest.clearAllMocks();
   });
 
   test('should render "You are not enrolled in any leagues" message when no leagues are found', async () => {
+    mockUseDataStore.mockReturnValueOnce({ user: { id: '123', leagues: [] } });
+    mockGetUserLeagues.mockResolvedValueOnce([]);
+    mockGetGameWeek.mockResolvedValueOnce({ week: 1 });
     render(<Leagues />);
 
     await waitFor(() => {
