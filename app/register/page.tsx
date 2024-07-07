9777134c1e98e0b736068b03cd5bdb1e658b06df
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2024-07-07 10:48:33 -0400

    fix(#273): add JSDoc comment

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) Gridiron Survivor.
 // Licensed under the MIT License.
 
-import { JSX} from 'react';
+import { JSX } from 'react';
 import { Metadata } from 'next';
 import Register from './Register';
 
@@ -10,6 +10,10 @@ export const metadata: Metadata = {
   description: 'Fantasy Football Survivor Pool',
 };
 
+/**
+ * Renders the registartion page.
+ * @returns {JSX.Element} The rendered registration page.
+ */
 const RegisterPage = (): JSX.Element => {
   return <Register />;
 };
