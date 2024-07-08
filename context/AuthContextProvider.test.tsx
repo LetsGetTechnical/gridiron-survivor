0658c842593b0ad525288225622e4708d55f32b6
Clue355 <dm.carlosluevano@gmail.com>
2024-07-08 16:28:16 -0500

    created better tests

--- context/AuthContextProvider.test.tsx
+++ context/AuthContextProvider.test.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import Alert from '@/components/AlertNotification/AlertNotification';
 import { AlertVariants } from '@/components/AlertNotification/Alerts.enum';
-import { render } from '@testing-library/react';
+import { render, waitFor, screen } from '@testing-library/react';
 import { toast } from 'react-hot-toast';
-import { logoutAccount } from './__mocks__/AuthContextProvider';
+import {
+  mockLogoutAuth,
+  mockLogoutErrorAuth,
+} from './__mocks__/AuthContextProvider';
+import Login from '@/app/login/page';
 
 const mockLoginAccount = jest.fn();
 
-jest.mock('./__mocks__/AuthContextProvider', () => ({
-  logoutAccount: jest.fn(),
-}));
+// jest.mock('./__mocks__/AuthContextProvider', () => ({
+//   logoutAccount: jest.fn(),
+// }));
 
 jest.mock('react-hot-toast', () => ({
   toast: {
@@ -22,17 +26,45 @@ describe('AuthContextProvider', () => {
     jest.clearAllMocks();
   });
 
-  test('mock a successful logout and show default notification', async () => {
-    (logoutAccount as jest.Mock).mockImplementation(async () => {
-      toast.custom(
-        <Alert variant={AlertVariants.Default} message="Logged Out" />,
-      );
-    });
+  // test('mock a successful logout and show default notification', async () => {
+  //   (logoutAccount as jest.Mock).mockImplementation(async () => {
+  //     toast.custom(
+  //       <Alert variant={AlertVariants.Default} message="Logged Out" />,
+  //     );
+  //   });
 
-    await logoutAccount();
-    expect(logoutAccount).toHaveBeenCalled();
+  //   await logoutAccount();
+  //   expect(logoutAccount).toHaveBeenCalled();
+  //   expect(toast.custom).toHaveBeenCalledWith(
+  //     <Alert variant={AlertVariants.Default} message="Logged Out" />,
+  //   );
+  // });
+
+  test('mock a successful logout and show default notification', async () => {
+    await mockLogoutAuth();
     expect(toast.custom).toHaveBeenCalledWith(
       <Alert variant={AlertVariants.Default} message="Logged Out" />,
     );
+  });
+
+  test('mock a Error when logging out and show default notification', async () => {
+    await mockLogoutErrorAuth();
+    expect(toast.custom).toHaveBeenCalledWith(
+      <Alert
+        variant={AlertVariants.Error}
+        message="Something went wrong. Try logging out again."
+      />,
+    );
+  });
+
+  test('mock an alert to show on the screen correctly', async () => {
+    await mockLogoutAuth();
+    render(<Login />);
+
+    const alertElement = screen.getByTestId('alert-icon');
+
+    await waitFor(() => {
+      expect(alertElement).toBeInTheDocument();
+    });
   });
 });
