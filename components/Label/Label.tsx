f0777a92042b04d8b854c0401497add35df7f9de
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2024-07-10 15:54:38 -0400

    fix(#273): remove unused code

--- components/Label/Label.tsx
+++ components/Label/Label.tsx
@@ -8,25 +8,6 @@ import * as LabelPrimitive from '@radix-ui/react-label';
 import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '../../utils/utils';
 
-// const labelVariants = cva(
-//   'text-base font-normal leading-none text-zinc-50 cursor-pointer flex gap-2 items-center rounded-xl border-2 border-zinc-800 peer-aria-checked:border-orange-600 py-4 px-3 w-full peer-hover:bg-zinc-800 transition',
-// );
-
-// const Label = React.forwardRef<
-//   React.ElementRef<typeof LabelPrimitive.Root>,
-//   React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
-//     VariantProps<typeof labelVariants>
-// >(({ className, ...props }, ref) => (
-//   <LabelPrimitive.Root
-//     ref={ref}
-//     className={cn(labelVariants(), className)}
-//     {...props}
-//   />
-// ));
-// Label.displayName = LabelPrimitive.Root.displayName;
-
-// export { Label };
-
 const labelVariants = cva(
   'text-base font-normal leading-none gap-2 w-full transition',
   {
@@ -44,7 +25,8 @@ const labelVariants = cva(
 
 const Label = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> & VariantProps<typeof labelVariants>
+  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> & 
+    VariantProps<typeof labelVariants>
 >(({ className, variant, ...props }, ref) => (
   <LabelPrimitive.Root
     ref={ref}
