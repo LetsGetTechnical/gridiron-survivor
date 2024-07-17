53d06811385442f5b81c5cdd43208f4d0e3776d3
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2024-07-17 14:50:51 -0400

    #426 FEAT: Add admin routes (#431)
    fixes #426 
    
    **NOTE**: This adds new routes for the the admin dashboards. However, in
    order to keep the layouts separate for admin pages vs game pages I had
    to update the folder structure. See below.
    
    
    ## SCREENSHOT
    ![CleanShot 2024-07-17 at 14 09
    42](https://github.com/user-attachments/assets/93fb42dd-4e4d-4a18-a1c3-38ba6e57cdd3)

---
+++ app/(admin)/admin/players/page.tsx
@@ -0,0 +1,14 @@
+// Copyright (c) Gridiron Survivor.
+// Licensed under the MIT License.
+
+import { JSX } from 'react';
+
+/**
+ * Renders the admin page.
+ * @returns {JSX.Element} - The rendered login page.
+ */
+const AdminPlayers = (): JSX.Element => {
+  return <div>Admin Players</div>;
+};
+
+export default AdminPlayers;
