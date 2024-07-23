b13771f97d0b427f88a59fe4829a4a3764eec3de
Ryan Furrer <ryandotfurrer@gmail.com>
2024-07-23 19:47:56 -0400

    fix(LeagueCard.tsx): added hover styles that were unintentionally removed

--- components/LeagueCard/LeagueCard.tsx
+++ components/LeagueCard/LeagueCard.tsx
@@ -26,7 +26,7 @@ const LeagueCard = React.forwardRef<HTMLAnchorElement, ILeagueCardProps>(
       data-testid="LeagueCard"
       href={href}
       className={cn(
-        'LeagueCard flex max-h-32 place-items-center gap-6 rounded-lg border bg-card p-4 text-card-foreground shadow-sm dark:border-zinc-800',
+        'LeagueCard flex max-h-32 place-items-center gap-6 rounded-lg  border bg-card p-4 text-card-foreground shadow-sm hover:bg-zinc-800 dark:border-zinc-800',
       )}
       ref={ref}
     >
