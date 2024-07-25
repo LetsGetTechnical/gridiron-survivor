caf8de3178af6b61c532f0d925d836d393f9b394
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2024-07-25 04:38:21 -0400

    fix(#273): remove data-testid from LinkCustom test

--- components/LinkCustom/LinkCustom.test.tsx
+++ components/LinkCustom/LinkCustom.test.tsx
@@ -5,9 +5,9 @@ import LinkCustom from './LinkCustom';
 describe('LinkCustom Component', () => {
   it('renders with default props', () => {
     render(
-      <LinkCustom children="Test link" data-testid="test-link" href="https://example.com"></LinkCustom>,
+      <LinkCustom children="Test link" href="https://example.com"></LinkCustom>,
     );
-    const link = screen.getByTestId('test-link');
+    const link = screen.getByTestId('linkCustom');
     expect(link).toBeInTheDocument();
     expect(link).toHaveTextContent('Test link');
     expect(link).toHaveAttribute('href', 'https://example.com');
