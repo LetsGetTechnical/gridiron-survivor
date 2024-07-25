0bc943b0d31e3597483146c6ac91fdd532b3783a
Ryan Furrer <ryandotfurrer@gmail.com>
2024-07-25 19:10:07 -0400

    test(GlobalSpinner.test.tsx): added unit test for the globalspinner component
    test checks that the globalspinnner component renders and has the expected src

---
+++ components/GlobalSpinner/GlobalSpinner.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import GlobalSpinner from './GlobalSpinner';
+import React from 'react';
+
+describe('GlobalSpinner', () => {
+  beforeEach(() => {
+    render(<GlobalSpinner />);
+  });
+  it('should render without errors', () => {
+    const globalSpinner = screen.getByTestId('global-spinner');
+    expect(globalSpinner).toBeInTheDocument();
+  });
+
+  it('should have the correct file path', () => {
+    const spinnerImage = screen.getByTestId('global-spinner-image');
+    const filePath = spinnerImage.getAttribute('src');
+    expect(filePath).toMatch('../../assets/global-spinner.svg');
+  });
+});
