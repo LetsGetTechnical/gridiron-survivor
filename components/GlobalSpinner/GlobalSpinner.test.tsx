ba5a5bd0b30884a0f569578fbcb9686af416fd3b
Ryan Furrer <ryandotfurrer@gmail.com>
2024-07-26 19:12:39 -0400

    fix globalspinner.test.tsx

--- components/GlobalSpinner/GlobalSpinner.test.tsx
+++ components/GlobalSpinner/GlobalSpinner.test.tsx
@@ -1,19 +1,45 @@
+import { ImageProps } from 'next/image';
 import { render, screen } from '@testing-library/react';
 import GlobalSpinner from './GlobalSpinner';
 import React from 'react';
+
+interface IMockNextImageProps {
+  alt: string;
+  height: number;
+  src: string;
+  width: number;
+}
+
+jest.mock('next/image', () => ({
+  __esModule: true,
+  default: (props: IMockNextImageProps) => {
+    const { alt, height, src, width }: IMockNextImageProps = props;
+    return (
+      <img
+        alt={alt}
+        data-testId="global-spinner-image"
+        height={100}
+        src="@/public/assets/global-spinner.svg"
+        width={100}
+      />
+    );
+  },
+}));
 
 describe('GlobalSpinner', () => {
   beforeEach(() => {
     render(<GlobalSpinner />);
   });
+
   it('should render without errors', () => {
     const globalSpinner = screen.getByTestId('global-spinner');
     expect(globalSpinner).toBeInTheDocument();
   });
 
-  it('should have the correct file path', () => {
-    const spinnerImage = screen.getByTestId('global-spinner-image');
-    const filePath = spinnerImage.getAttribute('src');
-    expect(filePath).toMatch('../../assets/global-spinner.svg');
+  it('should use the correct image source', () => {
+    const image = screen.getByTestId(
+      'global-spinner-image',
+    ) as HTMLImageElement;
+    expect(image.src).toContain('global-spinner');
   });
 });
