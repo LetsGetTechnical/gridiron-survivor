a984f28e367980116b6024d2c4c5cea23de5ac92
Clue355 <dm.carlosluevano@gmail.com>
2024-07-29 14:40:52 -0500

    alphabetized interface

--- context/AuthHelper.interface.ts
+++ context/AuthHelper.interface.ts
@@ -5,7 +5,7 @@ import React from 'react';
 import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
 
 export interface ILogoutType {
-  setIsSignedIn: React.Dispatch<React.SetStateAction<boolean>>;
   resetUser: React.Dispatch<React.SetStateAction<void>>;
   router: AppRouterInstance;
+  setIsSignedIn: React.Dispatch<React.SetStateAction<boolean>>;
 }
