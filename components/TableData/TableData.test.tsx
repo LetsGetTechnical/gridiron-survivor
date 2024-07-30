7864331bfdaeeb0731d511db24492423803e0270
Alex Appleget <alexappleget2014@gmail.com>
2024-07-30 13:58:28 -0500

    Fix: finished test for TableData component

--- components/TableData/TableData.test.tsx
+++ components/TableData/TableData.test.tsx
@@ -1,22 +1,33 @@
 import React from "react";
 import { screen, render } from "@testing-library/react";
 import TableData from "./TableData";
+import { ColumnDef } from "@tanstack/react-table";
 
-const columns = [
+type mockType = {
+    text: string;
+    text2: string;
+    text3: string;
+}
+
+const mockColumns: ColumnDef<mockType>[] = [
     {
+        accessorKey: 'text',
         header: 'Test Header',
-        cell: 'Test Cell',
+        cell: ({ row }) => <div>{row.getValue('text')}</div>,
     },
     {
+        accessorKey: 'text2',
         header: 'Test Header 2',
-        cell: 'Test Cell 2',
+        cell: ({ row }) => <div>{row.getValue('text2')}</div>,
     },
     {
+        accessorKey: 'text3',
         header: 'Test Header 3',
-        cell: 'Test Cell 3',
+        cell: ({ row }) => <div>{row.getValue('text3')}</div>,
     }
 ];
-const data = [
+
+const mockData = [
     {
         text: 'Test',
         text2: 'Test2',
@@ -26,14 +37,14 @@ const data = [
 
 describe('Table Component', () => {
     it('should render the table component with the columns and data when called', () => {
-        render(<TableData columns={columns} data={data} />);
+        render(<TableData columns={mockColumns} data={mockData} />);
 
-        columns.forEach((column) => {
-            const header = screen.getByText(column.header);
-            const cell = screen.getByText(column.cell);
+        expect(screen.getByText('Test Header')).toBeInTheDocument();
+        expect(screen.getByText('Test Header 2')).toBeInTheDocument();
+        expect(screen.getByText('Test Header 3')).toBeInTheDocument();
 
-            expect(header).toBeInTheDocument();
-            expect(cell).toBeInTheDocument();
-        });
+        expect(screen.getByText('Test')).toBeInTheDocument();
+        expect(screen.getByText('Test2')).toBeInTheDocument();
+        expect(screen.getByText('Test3')).toBeInTheDocument();
     })
 });
