66835c2cd339b1ccacb28119e6891962aaa1ab12
Ryan Furrer <ryandotfurrer@gmail.com>
2024-08-02 15:28:55 -0400

    feat((admin)/layout.tsx): began building out admin layout

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -8,7 +8,30 @@ import { JSX } from 'react';
  * @returns {JSX.Element} - The rendered login page.
  */
 const AdminHome = (): JSX.Element => {
-  return <div>Admin Home</div>;
+  return (
+    <section className="space-y-4">
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga minus
+        sunt culpa alias itaque. Praesentium expedita facere architecto at atque
+        rem voluptates temporibus iure earum amet. Quidem amet consequatur esse!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga minus
+        sunt culpa alias itaque. Praesentium expedita facere architecto at atque
+        rem voluptates temporibus iure earum amet. Quidem amet consequatur esse!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga minus
+        sunt culpa alias itaque. Praesentium expedita facere architecto at atque
+        rem voluptates temporibus iure earum amet. Quidem amet consequatur esse!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga minus
+        sunt culpa alias itaque. Praesentium expedita facere architecto at atque
+        rem voluptates temporibus iure earum amet. Quidem amet consequatur esse!
+      </p>
+    </section>
+  );
 };
 
 export default AdminHome;
--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,12 +1,16 @@
 // Copyright (c) Gridiron Survivor.
 // Licensed under the MIT License.
 
-import React, { JSX } from 'react';
-import { GeistSans } from 'geist/font/sans';
 import '../globals.css';
 import { AuthContextProvider } from '@/context/AuthContextProvider';
-import ErrorBoundary from '../error';
+import { GeistSans } from 'geist/font/sans';
 import { Toaster } from 'react-hot-toast';
+import ErrorBoundary from '../error';
+import React, { JSX } from 'react';
+import Link from 'next/link';
+import LogoNav from '@/components/LogoNav/LogoNav';
+import Heading from '@/components/Heading/Heading';
+import { LucideChevronsUpDown } from 'lucide-react';
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -31,10 +35,53 @@ const RootLayout = ({
 }): JSX.Element => {
   return (
     <html lang="en" className={GeistSans.className}>
-      <body className="dark:dark bg-background pb-8 px-4 text-foreground xl:pb-0">
+      <body className="dark:dark bg-background text-foreground">
         <ErrorBoundary>
           <AuthContextProvider>
-            <main>{children}</main>
+            <main className="h-screen">
+              <section className="h-screen">
+                <section className="w-1/5 border-r-2 border-border">
+                  <div className="px-2">
+                    <LogoNav />
+                  </div>
+                  <nav className="px-2">
+                    <ul>
+                      <li>
+                        <Link href="#">Home</Link>
+                      </li>
+                      <li>
+                        <Link href="#">Leauges</Link>
+                      </li>
+                      <li>
+                        <Link href="#">Players</Link>
+                      </li>
+                      <li>
+                        <Link href="#">Notifications</Link>
+                      </li>
+                    </ul>
+                    <div className="flex gap-2 px-2 py-2 items-center outline outline-border rounded">
+                      <span className="bg-cyan-500 w-8 h-8 rounded-full" />
+                      <p>Users Name</p>
+                      <LucideChevronsUpDown
+                        className="ml-auto text-zinc-300"
+                        size={16}
+                      />
+                    </div>
+                  </nav>
+                </section>
+                <section>
+                  <section>Admin Home 3</section>
+                  <header className="pb-8">
+                    <Heading as="h2">Admin Home 4</Heading>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                      Labore cumque tempora laborum velit perferendis tenetur.
+                    </p>
+                  </header>
+                  {children}
+                </section>
+              </section>
+            </main>
             <Toaster />
           </AuthContextProvider>
         </ErrorBoundary>
