6a4e5bf61a63a251aff009b0a3d1b84a7c30cbac
Alex Appleget <137813659+alexappleget@users.noreply.github.com>
2024-08-20 16:20:19 -0500

    Alex/Feat(#394): Design the table layout and pagination controls. (#452)
    Closes #394 
    
    Description:
    Create a visually appealing and functional table layout for displaying
    league data. Do not worry about pagination for now.
    
    Use ShadCn with Tanstack Table. This will achieve what we're looking
    for: https://ui.shadcn.com/docs/components/data-table
    
    Acceptance Criteria:
    
    - [x] Table design is consistent with admin interface and displays data
    clearly.
    
    
    Notes from Alex:
    
    - To test the table, I had to create my own static data and render the
    table. I did so inside leagues/page.tsx just for myself.
    
    - When we use this table, we will just have to create the function to
    grab the data, render <Table columns={columns} data={data} />, and then
    go inside the TableColumns/TableColumns.tsx and adjust it to grab the
    value of the responses from the function.
    
    
    
    https://github.com/user-attachments/assets/9c5fdeba-22b1-464c-8136-7c39f65615e2
    
    ![Screenshot 2024-07-10
    185957](https://github.com/user-attachments/assets/9b1d017d-8c2c-42e3-98e9-07cdaf8f2d8c)
    ![Screenshot 2024-07-23
    141007](https://github.com/user-attachments/assets/2562b0a6-9ff6-454b-bd3f-1c02053c9993)
    ![Screenshot 2024-07-23
    141206](https://github.com/user-attachments/assets/a558801e-fc93-4add-9802-8ec139e461a6)
    
    ---------
    
    Co-authored-by: Shashi Lo <362527+shashilo@users.noreply.github.com>

---
+++ components/TableData/TableData.test.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { ColumnDef } from '@tanstack/react-table';
+import TableData from './TableData';
+
+type mockType = {
+  text: string;
+  text2: string;
+  text3: string;
+};
+
+const mockColumns: ColumnDef<mockType>[] = [
+  {
+    accessorKey: 'text',
+    header: 'Test Header',
+    cell: ({ row }) => <div>{row.getValue('text')}</div>,
+  },
+  {
+    accessorKey: 'text2',
+    header: 'Test Header 2',
+    cell: ({ row }) => <div>{row.getValue('text2')}</div>,
+  },
+  {
+    accessorKey: 'text3',
+    header: 'Test Header 3',
+    cell: ({ row }) => <div>{row.getValue('text3')}</div>,
+  },
+];
+
+const mockData = [
+  {
+    text: 'Test',
+    text2: 'Test2',
+    text3: 'Test3',
+  },
+];
+
+describe('Table Component', () => {
+  it('should render the table component with the columns and data when called', () => {
+    render(<TableData columns={mockColumns} data={mockData} />);
+
+    expect(screen.getByText('Test Header')).toBeInTheDocument();
+    expect(screen.getByText('Test Header 2')).toBeInTheDocument();
+    expect(screen.getByText('Test Header 3')).toBeInTheDocument();
+
+    expect(screen.getByText('Test')).toBeInTheDocument();
+    expect(screen.getByText('Test2')).toBeInTheDocument();
+    expect(screen.getByText('Test3')).toBeInTheDocument();
+  });
+});
