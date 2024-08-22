7816397757ca3d4b2b084dfa94338e30e8094b28
Ryan Furrer <ryandotfurrer@gmail.com>
2024-08-21 20:59:36 -0400

    re-implement UI on league entries page

--- app/(main)/league/[leagueId]/entry/all/page.tsx
+++ app/(main)/league/[leagueId]/entry/all/page.tsx
@@ -4,18 +4,22 @@
 'use client';
 import {
   createEntry,
+  getCurrentLeague,
   getCurrentUserEntries,
   getGameWeek,
 } from '@/api/apiFunctions';
-import { useDataStore } from '@/store/dataStore';
-import React, { JSX, useEffect, useState } from 'react';
-import { IEntry, IEntryProps } from '../Entries.interface';
-import { LeagueEntries } from '@/components/LeagueEntries/LeagueEntries';
-import { ENTRY_URL, LEAGUE_URL, WEEK_URL } from '@/const/global';
-import { IGameWeek } from '@/api/apiFunctions.interface';
 import { Button } from '@/components/Button/Button';
-import { PlusCircle } from 'lucide-react';
+import { ChevronLeft, PlusCircle } from 'lucide-react';
+import { ENTRY_URL, LEAGUE_URL, WEEK_URL } from '@/const/global';
+import { IEntry, IEntryProps } from '../Entries.interface';
+import { IGameWeek } from '@/api/apiFunctions.interface';
+import { LeagueEntries } from '@/components/LeagueEntries/LeagueEntries';
+import { LeagueSurvivors } from '@/components/LeagueSurvivors/LeagueSurvivors';
+import { useDataStore } from '@/store/dataStore';
 import GlobalSpinner from '@/components/GlobalSpinner/GlobalSpinner';
+import Heading from '@/components/Heading/Heading';
+import Link from 'next/link';
+import React, { JSX, useEffect, useState } from 'react';
 
 /**
  * Display all entries for a league.
@@ -29,8 +33,33 @@ const Entry = ({
 }): JSX.Element => {
   const [currentWeek, setCurrentWeek] = useState<IGameWeek['week']>(1);
   const [entries, setEntries] = useState<IEntry[]>([]);
+  const [leagueName, setLeagueName] = useState<string>('');
   const [loadingData, setLoadingData] = useState<boolean>(true);
+  const [survivors, setSurvivors] = useState<number>(0);
+  const [totalPlayers, setTotalPlayers] = useState<number>(0);
   const { user } = useDataStore((state) => state);
+
+  /**
+   * Fetches the current league name.
+   * @returns {Promise<void>}
+   */
+  useEffect(() => {
+    /**
+     * Fetches the current league name.
+     */
+    const getCurrentLeagueName = async (): Promise<void> => {
+      try {
+        const league = await getCurrentLeague(leagueId);
+        setLeagueName(league.leagueName);
+        setSurvivors(league.survivors.length);
+        setTotalPlayers(league.participants.length);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getCurrentLeagueName();
+  }, [leagueId]);
 
   /**
    * Fetches all entries for the current user.
@@ -87,7 +116,6 @@ const Entry = ({
     if (!user.id || user.id === '') {
       return;
     }
-
     getCurrentGameWeek();
     getAllEntries();
   }, [user]);
@@ -97,9 +125,35 @@ const Entry = ({
       {loadingData ? (
         <GlobalSpinner />
       ) : (
-        <>
+        <div className="mx-auto max-w-3xl pt-10">
+          <header>
+            <div className="to-leagues-link">
+              <Link
+                className="text-xl text-orange-600 hover:text-orange-500 flex items-center gap-3 font-semibold hover:underline"
+                href={`/league/all`}
+              >
+                <ChevronLeft />
+                Your Leagues
+              </Link>
+            </div>
+            <div className="entries-header-main flex flex-col justify-between text-center gap-10 pt-6 pb-4">
+              <div className="entries-hader-name flex flex-col gap-3">
+                <Heading as="h2" className="text-4xl font-bold">
+                  {leagueName}
+                </Heading>
+                <LeagueSurvivors
+                  className="text-lg font-semibold"
+                  survivors={survivors}
+                  totalPlayers={totalPlayers}
+                />
+              </div>
+              <Heading as="h3" className="text-2xl leading-8 font-semibold">
+                Week {currentWeek}
+              </Heading>
+            </div>
+          </header>
           {entries.length > 0 ? (
-            <>
+            <section className="flex flex-col gap-3">
               {entries.map((entry) => {
                 const linkUrl = `/${LEAGUE_URL}/${leagueId}/${ENTRY_URL}/${entry.$id}/${WEEK_URL}/${currentWeek}`;
                 const isPickSet =
@@ -123,7 +177,7 @@ const Entry = ({
                 );
               })}
 
-              <div className="flex justify-center items-center mt-2 mb-2 w-full">
+              <div className="flex flex-col gap-8 justify-center items-center mt-2 mb-2 w-full">
                 <Button
                   icon={<PlusCircle className="mr-2" />}
                   variant="outline"
@@ -137,8 +191,16 @@ const Entry = ({
                 >
                   Add New Entry
                 </Button>
+                {currentWeek > 1 && (
+                  <Link
+                    className="text-orange-600 hover:text-orange-500 font-bold hover:underline"
+                    href={`#`}
+                  >
+                    View Past Weeks
+                  </Link>
+                )}
               </div>
-            </>
+            </section>
           ) : (
             <div className="flex justify-center items-center mt-2 mb-2 w-full">
               <Button
@@ -156,7 +218,7 @@ const Entry = ({
               </Button>
             </div>
           )}
-        </>
+        </div>
       )}
     </>
   );
