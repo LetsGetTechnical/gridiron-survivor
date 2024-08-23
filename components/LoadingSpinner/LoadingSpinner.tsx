7e0979659b12fceee12cf9c66da5d6acb616ca87
Clue355 <dm.carlosluevano@gmail.com>
2024-08-23 14:14:51 -0500

    deleted duplicate code

--- components/LoadingSpinner/LoadingSpinner.tsx
+++ components/LoadingSpinner/LoadingSpinner.tsx
@@ -14,8 +14,6 @@ const LoadingSpinner = (): JSX.Element => {
       data-testid="loading-spinner"
       role="alert"
       tabIndex={-1}
-      className="loading-spinner-container grid place-content-center text-center"
-      data-testid="loading-spinner"
     >
       <div className="loading-spinner min-w-[1.75rem] max-w-[2.25rem] min-h-[1.75rem] max-h-[2.25rem] rounded-full border-4 border-t-muted-foreground border-foreground animate-spin motion-reduce:hidden" />
       <p className="sr-only motion-reduce:not-sr-only">Loading...</p>
