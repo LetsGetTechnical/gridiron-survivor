8840b9d1232e8ab57d2fe9812cb891d347ecbf24
Clue355 <dm.carlosluevano@gmail.com>
2024-08-28 18:01:34 -0500

    finshed implementing admin menu dropdown

--- components/AdminUserSettings/AdminUserSettings.test.tsx
+++ components/AdminUserSettings/AdminUserSettings.test.tsx
@@ -1,12 +1,62 @@
 import { AdminUserSettings } from './AdminUserSettings';
-import { render, screen } from '@testing-library/react';
+import {
+  fireEvent,
+  getByTestId,
+  render,
+  screen,
+  waitFor,
+} from '@testing-library/react';
 import React from 'react';
 
+const mockLogoutAccount = jest.fn();
+
+const mockUseAuthContext = {
+  logoutAccount: mockLogoutAccount,
+  isSignedIn: true,
+};
+
+jest.mock('../../context/AuthContextProvider', () => ({
+  useAuthContext() {
+    return {
+      ...mockUseAuthContext,
+    };
+  },
+}));
+
 describe('AdminUserSettings Component', () => {
-  it('should render the component', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+
     render(<AdminUserSettings />);
+  });
+
+  it('should render the component', async () => {
     const adminUserSettings = screen.getByTestId('admin-user-settings');
 
     expect(adminUserSettings).toBeInTheDocument();
+  });
+
+  it('should show user options when clicked', () => {
+    const adminUserSettings = screen.getByTestId('admin-user-settings');
+
+    fireEvent.click(adminUserSettings);
+
+    waitFor(() => {
+      expect(screen.getByTestId('edit-profile-button')).toBeInTheDocument();
+      expect(screen.getByTestId('sign-out-button')).toBeInTheDocument();
+    });
+  });
+
+  it('should not show user options when closed', () => {
+    const adminUserSettings = screen.getByTestId('admin-user-settings');
+
+    fireEvent.click(adminUserSettings);
+
+    fireEvent.click(adminUserSettings);
+
+    waitFor(() => {
+      expect(screen.getByTestId('edit-profile-button')).not.toBeInTheDocument();
+      expect(screen.getByTestId('sign-out-button')).not.toBeInTheDocument();
+    });
   });
 });
--- components/AdminUserSettings/AdminUserSettings.tsx
+++ components/AdminUserSettings/AdminUserSettings.tsx
@@ -1,22 +1,68 @@
+'use client';
 // Copyright (c) Gridiron Survivor.
 // Licensed under the MIT License.
 
+import { Button } from '../Button/Button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '../TableDropDownMenu/TableDropDownMenu';
 import { LucideChevronsUpDown } from 'lucide-react';
 import React from 'react';
+import { redirect } from 'next/navigation';
+import { useAuthContext } from '@/context/AuthContextProvider';
 
 /**
  * The admin user settings component.
  * @returns The rendered admin user settings.
  */
 export const AdminUserSettings = (): React.JSX.Element => {
+  const { logoutAccount } = useAuthContext();
+
+  /**
+   * Handles the logout.
+   * @returns {Promise<void>} The logout promise.
+   */
+  const handleLogout = async (): Promise<void> => {
+    try {
+      await logoutAccount();
+      redirect('/login');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div
-      className="admin-user-settings flex gap-2 px-2 py-2 items-center outline outline-border rounded text-zinc-50"
-      data-testid="admin-user-settings"
-    >
-      <span className="bg-cyan-500 w-8 h-8 rounded-full" />
-      <p>Users Name</p>
-      <LucideChevronsUpDown className="ml-auto text-zinc-300" size={16} />
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger className="w-full" data-testid="admin-user-settings">
+        <div className="admin-user-settings flex gap-2 px-2 py-2 items-center outline outline-border rounded text-zinc-50">
+          <span className="bg-cyan-500 w-8  h-8 rounded-full" />
+          <p>Users Name</p>
+          <LucideChevronsUpDown className="ml-auto text-zinc-300" size={16} />
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="outline outline-border rounded border-zinc-700 w-[232px]">
+        <DropdownMenuItem className="cursor-pointer">
+          <Button
+            className="flex justify-start p-0 text-base font-normal hover:no-underline w-full"
+            variant="link"
+            label="Edit Profile"
+            onClick={handleLogout}
+            data-testid="edit-profile-button"
+          />
+        </DropdownMenuItem>
+        <DropdownMenuItem className="cursor-pointer">
+          <Button
+            className="flex justify-start p-0 text-base font-normal hover:no-underline w-full"
+            variant="link"
+            label="Sign Out"
+            onClick={handleLogout}
+            data-testid="sign-out-button"
+          />
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
