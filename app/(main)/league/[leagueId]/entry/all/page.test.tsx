3978c0c5e4b2ffad92fea99e0a3a83a1e464b931
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2024-09-04 22:03:58 -0400

    #496 Chris/Limit-User-Entries (#498)
    fixes #496 
    
    ✅ Limits user to 5 entries
    ✅ Updated Unit Test
    
    ## SCREENSHOT
    ![CleanShot_2024-09-04_at_13 49
    55](https://github.com/user-attachments/assets/306e60ea-15b9-4be3-b1fc-dbf1d97ba1df)

--- app/(main)/league/[leagueId]/entry/all/page.test.tsx
+++ app/(main)/league/[leagueId]/entry/all/page.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor, cleanup } from '@testing-library/react';
 import Entry from './page';
 import { useDataStore } from '@/store/dataStore';
 import {
@@ -19,15 +19,7 @@ jest.mock('@/api/apiFunctions', () => ({
       survivors: 10,
     }),
   ),
-  getCurrentUserEntries: jest.fn(() =>
-    Promise.resolve([
-      {
-        id: '123',
-        week: 1,
-        selectedTeams: [],
-      },
-    ]),
-  ),
+  getCurrentUserEntries: jest.fn(),
   getGameWeek: jest.fn(() =>
     Promise.resolve({
       week: 1,
@@ -46,14 +38,26 @@ describe('League entries page (Entry Component)', () => {
   });
 
   test('should display GlobalSpinner while loading data', async () => {
-    mockUseDataStore.mockReturnValueOnce({ user: { id: '123', leagues: [] } });
+    mockUseDataStore.mockReturnValueOnce({
+      user: { id: '123', leagues: [] },
+    });
     mockGetGameWeek.mockResolvedValueOnce({ week: 1 });
     mockGetCurrentUserEntries.mockResolvedValueOnce([
       {
-        id: '123',
-        week: 1,
+        $id: '66311a210039f0532044',
+        name: 'Entry 1',
+        user: '1',
+        league: '1',
+        selectedTeams: '',
+        eliminated: false,
       },
     ]);
+    mockGetCurrentLeague.mockResolvedValueOnce({
+      leagueName: 'GiS League',
+      participants: 47,
+      survivors: 47,
+    });
+
     render(<Entry params={{ leagueId: '123' }} />);
 
     await waitFor(() => {
@@ -66,16 +70,29 @@ describe('League entries page (Entry Component)', () => {
     mockGetGameWeek.mockResolvedValueOnce({ week: 1 });
     mockGetCurrentUserEntries.mockResolvedValueOnce([
       {
-        id: '123',
-        week: 1,
+        $id: '66311a210039f0532044',
+        name: 'Entry 1',
+        user: '1',
+        league: '1',
+        selectedTeams: '',
+        eliminated: false,
       },
     ]);
+    mockGetCurrentLeague.mockResolvedValueOnce({
+      leagueName: 'GiS League',
+      participants: 47,
+      survivors: 47,
+    });
 
     render(<Entry params={{ leagueId: '123' }} />);
 
     await waitFor(() => {
-      expect(screen.queryByTestId('global-spinner')).not.toBeInTheDocument();
+      expect(mockGetGameWeek).toHaveBeenCalled();
+      expect(mockGetCurrentUserEntries).toHaveBeenCalled();
+      expect(mockGetCurrentLeague).toHaveBeenCalled();
     });
+
+    expect(screen.queryByTestId('global-spinner')).not.toBeInTheDocument();
   });
 
   it('should display the header with the league name, survivors, and week number, without a past weeks link', async () => {
@@ -83,8 +100,12 @@ describe('League entries page (Entry Component)', () => {
     mockGetGameWeek.mockResolvedValueOnce({ week: 1 });
     mockGetCurrentUserEntries.mockResolvedValueOnce([
       {
-        id: '66311a210039f0532044',
-        week: 1,
+        $id: '66311a210039f0532044',
+        name: 'Entry 1',
+        user: '1',
+        league: '1',
+        selectedTeams: '',
+        eliminated: false,
       },
     ]);
     mockGetCurrentLeague.mockResolvedValueOnce({
@@ -96,41 +117,46 @@ describe('League entries page (Entry Component)', () => {
     render(<Entry params={{ leagueId: '66311a210039f0532044' }} />);
 
     await waitFor(() => {
-      const entryPageHeader = screen.getByTestId('entry-page-header');
-      const entryPageHeaderToLeaguesLink = screen.getByTestId(
-        'entry-page-header-to-leagues-link',
-      );
-      const entryPageHeaderLeagueName = screen.getByTestId(
-        'entry-page-header-league-name',
-      );
-      const entryPageHeaderLeagueSurvivors =
-        screen.getByTestId('LeagueSurvivors');
-      const entryPageHeaderCurrentWeek = screen.getByTestId(
-        'entry-page-header-current-week',
-      );
-
-      expect(entryPageHeader).toBeInTheDocument();
-      expect(entryPageHeaderToLeaguesLink).toBeInTheDocument();
-      expect(entryPageHeaderToLeaguesLink).toHaveAttribute(
-        'href',
-        '/league/all',
-      );
-      expect(entryPageHeaderLeagueName).toBeInTheDocument();
-      expect(entryPageHeaderLeagueName).toHaveTextContent('GiS League');
-      expect(entryPageHeaderLeagueSurvivors).toBeInTheDocument();
-      expect(entryPageHeaderLeagueSurvivors).toHaveTextContent('Survivors');
-      expect(entryPageHeaderCurrentWeek).toBeInTheDocument();
-      expect(entryPageHeaderCurrentWeek).toHaveTextContent('Week 1');
+      expect(mockGetGameWeek).toHaveBeenCalled();
+      expect(mockGetCurrentUserEntries).toHaveBeenCalled();
+      expect(mockGetCurrentLeague).toHaveBeenCalled();
     });
+
+    const entryPageHeader = screen.getByTestId('entry-page-header');
+    const entryPageHeaderToLeaguesLink = screen.getByTestId(
+      'entry-page-header-to-leagues-link',
+    );
+    const entryPageHeaderLeagueName = screen.getByTestId(
+      'entry-page-header-league-name',
+    );
+    const entryPageHeaderLeagueSurvivors =
+      screen.getByTestId('LeagueSurvivors');
+    const entryPageHeaderCurrentWeek = screen.getByTestId(
+      'entry-page-header-current-week',
+    );
+
+    expect(entryPageHeader).toBeInTheDocument();
+    expect(entryPageHeaderToLeaguesLink).toBeInTheDocument();
+    expect(entryPageHeaderToLeaguesLink).toHaveAttribute('href', '/league/all');
+    expect(entryPageHeaderLeagueName).toBeInTheDocument();
+    expect(entryPageHeaderLeagueName).toHaveTextContent('GiS League');
+    expect(entryPageHeaderLeagueSurvivors).toBeInTheDocument();
+    expect(entryPageHeaderLeagueSurvivors).toHaveTextContent('Survivors');
+    expect(entryPageHeaderCurrentWeek).toBeInTheDocument();
+    expect(entryPageHeaderCurrentWeek).toHaveTextContent('Week 1');
   });
 
-  it('should display the header with the league name, survivors, and week number, with a past weeks link', async () => {
+  it('should display the header with the league name, survivors, and week number, with a past weeks link and add new entry button', async () => {
     mockUseDataStore.mockReturnValueOnce({ user: { id: '123', leagues: [] } });
     mockGetGameWeek.mockResolvedValueOnce({ week: 2 });
     mockGetCurrentUserEntries.mockResolvedValueOnce([
       {
-        id: '66311a210039f0532044',
-        week: 2,
+        $id: '66311a210039f0532044',
+        name: 'Entry 1',
+        user: '1',
+        league: '1',
+        selectedTeams: '',
+        eliminated: false,
       },
     ]);
     mockGetCurrentLeague.mockResolvedValueOnce({
@@ -142,33 +168,101 @@ describe('League entries page (Entry Component)', () => {
     render(<Entry params={{ leagueId: '66311a210039f0532044' }} />);
 
     await waitFor(() => {
-      const entryPageHeader = screen.getByTestId('entry-page-header');
-      const entryPageHeaderToLeaguesLink = screen.getByTestId(
-        'entry-page-header-to-leagues-link',
-      );
-      const entryPageHeaderLeagueName = screen.getByTestId(
-        'entry-page-header-league-name',
-      );
-      const entryPageHeaderLeagueSurvivors =
-        screen.getByTestId('LeagueSurvivors');
-      const entryPageHeaderCurrentWeek = screen.getByTestId(
-        'entry-page-header-current-week',
-      );
-      const viewPastWeeksLink = screen.getByTestId('past-weeks-link');
-
-      expect(entryPageHeader).toBeInTheDocument();
-      expect(entryPageHeaderToLeaguesLink).toBeInTheDocument();
-      expect(entryPageHeaderToLeaguesLink).toHaveAttribute(
-        'href',
-        '/league/all',
-      );
-      expect(entryPageHeaderLeagueName).toBeInTheDocument();
-      expect(entryPageHeaderLeagueName).toHaveTextContent('GiS League');
-      expect(entryPageHeaderLeagueSurvivors).toBeInTheDocument();
-      expect(entryPageHeaderLeagueSurvivors).toHaveTextContent('Survivors');
-      expect(entryPageHeaderCurrentWeek).toBeInTheDocument();
-      expect(entryPageHeaderCurrentWeek).toHaveTextContent('Week 2');
-      expect(viewPastWeeksLink).toBeInTheDocument();
+      expect(mockGetGameWeek).toHaveBeenCalled();
+      expect(mockGetCurrentUserEntries).toHaveBeenCalled();
+      expect(mockGetCurrentLeague).toHaveBeenCalled();
     });
+
+    const entryPageHeader = screen.getByTestId('entry-page-header');
+    const entryPageHeaderToLeaguesLink = screen.getByTestId(
+      'entry-page-header-to-leagues-link',
+    );
+    const entryPageHeaderLeagueName = screen.getByTestId(
+      'entry-page-header-league-name',
+    );
+    const entryPageHeaderLeagueSurvivors =
+      screen.getByTestId('LeagueSurvivors');
+    const entryPageHeaderCurrentWeek = screen.getByTestId(
+      'entry-page-header-current-week',
+    );
+    const viewPastWeeksLink = screen.getByTestId('past-weeks-link');
+    const addNewEntryButton = screen.getByTestId('add-new-entry-button');
+
+    expect(entryPageHeader).toBeInTheDocument();
+    expect(entryPageHeaderToLeaguesLink).toBeInTheDocument();
+    expect(entryPageHeaderToLeaguesLink).toHaveAttribute('href', '/league/all');
+    expect(entryPageHeaderLeagueName).toBeInTheDocument();
+    expect(entryPageHeaderLeagueName).toHaveTextContent('GiS League');
+    expect(entryPageHeaderLeagueSurvivors).toBeInTheDocument();
+    expect(entryPageHeaderLeagueSurvivors).toHaveTextContent('Survivors');
+    expect(entryPageHeaderCurrentWeek).toBeInTheDocument();
+    expect(entryPageHeaderCurrentWeek).toHaveTextContent('Week 2');
+    expect(addNewEntryButton).toBeInTheDocument();
+    expect(viewPastWeeksLink).toBeInTheDocument();
+  });
+
+  it('should not display a button to add a new entry if there are more than 5 entries', async () => {
+    mockUseDataStore.mockReturnValueOnce({ user: { id: '123', leagues: [] } });
+    mockGetGameWeek.mockResolvedValueOnce({ week: 2 });
+    mockGetCurrentUserEntries.mockResolvedValueOnce([
+      {
+        $id: '66311a210039f0532044',
+        name: 'Entry 1',
+        user: '1',
+        league: '1',
+        selectedTeams: '',
+        eliminated: false,
+      },
+      {
+        $id: '66311a210039f0532045',
+        name: 'Entry 2',
+        user: '1',
+        league: '1',
+        selectedTeams: '',
+        eliminated: false,
+      },
+      {
+        $id: '66311a210039f0532046',
+        name: 'Entry 3',
+        user: '1',
+        league: '1',
+        selectedTeams: '',
+        eliminated: false,
+      },
+      {
+        $id: '66311a210039f0532047',
+        name: 'Entry 4',
+        user: '1',
+        league: '1',
+        selectedTeams: '',
+        eliminated: false,
+      },
+      {
+        $id: '66311a210039f0532048',
+        name: 'Entry 5',
+        user: '1',
+        league: '1',
+        selectedTeams: '',
+        eliminated: false,
+      },
+    ]);
+    mockGetCurrentLeague.mockResolvedValueOnce({
+      leagueName: 'GiS League',
+      participants: 47,
+      survivors: 47,
+    });
+
+    render(<Entry params={{ leagueId: '66311a210039f0532044' }} />);
+
+    // Wait for the component to render
+    await waitFor(() => {
+      expect(mockGetGameWeek).toHaveBeenCalled();
+      expect(mockGetCurrentUserEntries).toHaveBeenCalled();
+      expect(mockGetCurrentLeague).toHaveBeenCalled();
+    });
+
+    expect(
+      screen.queryByTestId('add-new-entry-button'),
+    ).not.toBeInTheDocument();
   });
 });
