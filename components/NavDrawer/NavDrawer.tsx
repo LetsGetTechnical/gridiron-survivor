4dc1e4bcb0026fc4b3f155148550dfdf14bc29f6
Ryan Furrer <ryandotfurrer@gmail.com>
2024-09-07 20:50:58 -0400

    update styles in navdrawer component

--- components/NavDrawer/NavDrawer.tsx
+++ components/NavDrawer/NavDrawer.tsx
@@ -43,7 +43,7 @@ const DrawerOverlay = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DrawerPrimitive.Overlay
     ref={ref}
-    className={cn('fixed inset-0 z-50 bg-black/80', className)}
+    className={cn('fixed inset-0 z-50 bg-background/80', className)}
     {...props}
   />
 ));
