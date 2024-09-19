068061047bbe1c5b96621d3c3e5113fe6e20553d
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2024-09-19 10:42:26 -0400

    fix: bug new week shows previous week as current pick

--- app/(main)/league/[leagueId]/entry/[entryId]/week/Week.tsx
+++ app/(main)/league/[leagueId]/entry/[entryId]/week/Week.tsx
@@ -273,23 +273,23 @@ const Week = ({ entry, league, NFLTeams, week }: IWeekProps): JSX.Element => {
               Week {week} pick
             </h1>
 
-            {pickHistory.length > 0 && (
-              <section
-                className="flex flex-wrap w-[90%] gap-4 overflow-x-scroll justify-center pb-10 items-center"
-                data-testid="user-pick-history"
-              >
-                {pickHistory?.map((logoURL, index) => (
+            <div className="flex flex-wrap justify-center gap-2 mb-8">
+              {pickHistory?.map((logoURL, index) => {
+                const isCurrentWeek = index === pickHistory.length - 1;
+                const hasCurrentWeekPick = pickHistory.length === Number(week);
+
+                return (
                   <div
                     key={`${logoURL}-${index + 1}`}
                     className={cn(
-                      'flex flex-col items-center justify-center border  p-2 rounded-lg gap-1',
-                      index === pickHistory.length - 1
+                      'flex flex-col items-center justify-center border p-2 rounded-lg gap-1',
+                      isCurrentWeek && hasCurrentWeekPick
                         ? 'border-primary'
                         : 'border-border opacity-80',
                     )}
                   >
                     <span className="text-sm">
-                      {index === pickHistory.length - 1
+                      {isCurrentWeek && hasCurrentWeekPick
                         ? 'CURRENT'
                         : `WEEK ${index + 1}`}
                     </span>
@@ -302,9 +302,9 @@ const Week = ({ entry, league, NFLTeams, week }: IWeekProps): JSX.Element => {
                       alt="teamLogo"
                     />
                   </div>
-                ))}
-              </section>
-            )}
+                );
+              })}
+            </div>
 
             <FormProvider {...form}>
               <form className="mx-auto flex w-[90%] max-w-3xl flex-col items-center">
