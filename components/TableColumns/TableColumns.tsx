12defd8b70ece0259197d82cf849372d9680fec3
Alex Appleget <alexappleget2014@gmail.com>
2024-09-21 10:57:51 -0500

    Fix: Created League Details columns function.

--- components/TableColumns/TableColumns.tsx
+++ components/TableColumns/TableColumns.tsx
@@ -21,7 +21,138 @@ export type LeagueHeader = {
   text4: string;
 };
 
-export const entryColumns: ColumnDef<Header>[] = [];
+export type LeagueDetailsHeader = {
+  text: string;
+  text2: string;
+  text3: string;
+  text4: string;
+};
+
+export const leagueDetailsColumns: ColumnDef<LeagueDetailsHeader>[] = [
+  {
+    accessorKey: 'text',
+    header: 'User',
+    /**
+     * Value of row.
+     * @param {object} row - The row data.
+     * @param {object} row.row - The row definition
+     * @returns {JSX.Element} - The cell component.
+     */
+    cell: ({ row }) => <div>{row.getValue('text')}</div>,
+  },
+  {
+    accessorKey: 'text2',
+
+    /**
+     * Value of row.
+     * @param {object} column - The column data.
+     * @param {object} column.column - The column definition
+     * @returns {JSX.Element} - The cell component.
+     */
+    header: ({ column }): JSX.Element => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Entry #
+          <ChevronsUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+
+    /**
+     * Value of row.
+     * @param {object} row - The row data.
+     * @param {object} row.row - The row definition
+     * @returns {JSX.Element} - The cell component.
+     */
+    cell: ({ row }) => <div>{row.getValue('text2')}</div>,
+  },
+  {
+    accessorKey: 'text3',
+
+    /**
+     * Value of row.
+     * @param {object} column - The column data.
+     * @param {object} column.column - The column definition
+     * @returns {JSX.Element} - The cell component.
+     */
+    header: ({ column }): JSX.Element => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Pick Made
+          <ChevronsUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+
+    /**
+     * Value of row.
+     * @param {object} row - The row data.
+     * @param {object} row.row - The row definition
+     * @returns {JSX.Element} - The cell component.
+     */
+    cell: ({ row }) => <div>{row.getValue('text3')}</div>,
+  },
+  {
+    accessorKey: 'text4',
+
+    /**
+     * Value of row.
+     * @param {object} column - The column data.
+     * @param {object} column.column - The column definition
+     * @returns {JSX.Element} - The cell component.
+     */
+    header: ({ column }): JSX.Element => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Surviving?
+          <ChevronsUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+
+    /**
+     * Value of row.
+     * @param {object} row - The row data.
+     * @param {object} row.row - The row definition
+     * @returns {JSX.Element} - The cell component.
+     */
+    cell: ({ row }) => <div>{row.getValue('text2')}</div>,
+  },
+  {
+    id: 'actions',
+
+    /**
+     * Admin action dropdown.
+     * @returns {JSX.Element} - The cell component.
+     */
+    cell: (): JSX.Element => {
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem>View</DropdownMenuItem>
+            <DropdownMenuItem>Edit</DropdownMenuItem>
+            <DropdownMenuItem>Delete</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
 
 export const leagueColumns: ColumnDef<LeagueHeader>[] = [
   {
