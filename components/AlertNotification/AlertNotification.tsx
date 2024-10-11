3d2c998fe31a3812082c2ae2eb085e829f26d209
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-10-11 12:06:49 -0600

    add button component to alert

--- components/AlertNotification/AlertNotification.tsx
+++ components/AlertNotification/AlertNotification.tsx
@@ -5,9 +5,10 @@ import { Alert as AlertDefault } from '../Alert/Alert';
 import { AlertTitle } from '../AlertTItle/AlertTitle';
 import { AlertDescription } from '../AlertDescription/AlertDescription';
 import { JSX } from 'react';
-import { CheckCircle, XCircle, Info, AlertTriangle } from 'lucide-react';
+import { CheckCircle, XCircle, Info, AlertTriangle, X } from 'lucide-react';
 import { IAlertNotification } from './AlertNotification.interface';
 import { AlertVariants } from './Alerts.enum';
+import { Button } from '../Button/Button';
 
 const variantConfig = {
   success: {
@@ -46,6 +47,14 @@ const Alert = ({
       <Icon className="h-4 w-4" data-testid="alert-icon" />
       <AlertTitle title={title} data-testid="alert-title" />
       <AlertDescription message={message} data-testid="alert-message" />
+      <Button
+        variant="ghost"
+        size="icon"
+        className="absolute right-4 top-2"
+        data-testid="dismiss-alert-btn"
+      >
+        <X />
+      </Button>
     </AlertDefault>
   );
 };
