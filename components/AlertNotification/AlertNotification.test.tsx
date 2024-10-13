0bb4542872bea56ae6a1529406460d16f8d86bdc
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-10-13 08:46:34 -0600

    fix: address TypeScript error and begin drafting tests for dismiss button

--- components/AlertNotification/AlertNotification.test.tsx
+++ components/AlertNotification/AlertNotification.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import Alert from './AlertNotification';
 import { AlertVariants } from './Alerts.enum';
 import { CheckCircle, XCircle, Info, AlertTriangle } from 'lucide-react';
@@ -30,7 +30,28 @@ const variantTestCases = {
 describe('AlertNotification', () => {
   for (const [key, value] of Object.entries(variantTestCases)) {
     it(`renders the correct variant ${key}`, () => {
-      render(<Alert variant={AlertVariants[key]} message={value.message} />);
+      render(
+        <Alert
+          variant={AlertVariants[key as keyof typeof AlertVariants]}
+          message={value.message}
+        />,
+      );
     });
-  }
+  };
+
+  it('should render the dismiss button', () => {
+    // do I need to render the Alert first?
+    const dismissButton = screen.getByTestId('dismiss-alert-btn');
+    expect(dismissButton).toBeInTheDocument();
+  });
+
+  it('should close the notification upon clicking the dismiss button', 
+    // do I need to render the Alert first?
+    async () => {
+      const dismissButton = screen.getByTestId('dismiss-alert-btn');
+      fireEvent.click(dismissButton);
+      await waitFor(() => {
+        expect(dismissButton.getAttribute('data-state')).toBe('closed'); // how do I tell if Alert is present or not?
+      });
+  })
 });
