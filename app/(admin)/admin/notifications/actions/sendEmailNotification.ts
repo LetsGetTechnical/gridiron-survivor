e55c39fe9c1d7cbbd5ae2167a827e91c9720e748
Alex Appleget <alexappleget2014@gmail.com>
2024-11-06 11:53:33 -0600

    Fix: Created an api function to convert userIDs into their targetIDs for bcc emailing.

--- app/(admin)/admin/notifications/actions/sendEmailNotification.ts
+++ app/(admin)/admin/notifications/actions/sendEmailNotification.ts
@@ -3,7 +3,7 @@
 
 'use server';
 import { ID } from 'appwrite';
-import { messaging } from '@/api/serverConfig';
+import { messaging, users } from '@/api/serverConfig';
 
 /**
  * Function to send email.
@@ -27,12 +27,36 @@ export const sendEmailNotifications = async ({
       subject,
       content,
       [],
+      ['672a3534000502297d88'],
+      [],
+      [],
       groupUsers,
-      [],
-      [],
-      [],
     );
   } catch (error) {
-    throw new Error('Error Sending Email');
+    throw error;
   }
 };
+
+/**
+ * Function to take the userIDs and grab their targetIDs for emailing.
+ * @param props - userIDs
+ * @param props.userIDs - All the passed in userIDs.
+ * @returns {Promise<void>}
+ */
+export async function getUserTargets({
+  userIDs,
+}: {
+  userIDs: string[];
+}): Promise<string[]> {
+  try {
+    const userTargets = await Promise.all(
+      userIDs.map(async (userID) => {
+        const userTarget = await users.get(userID);
+        return userTarget.targets.map((target) => target.$id);
+      }),
+    );
+    return userTargets.flat();
+  } catch (error) {
+    throw error;
+  }
+}
