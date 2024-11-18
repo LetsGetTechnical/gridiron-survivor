2375c477c6315795fe38fb34781b60640e02727c
Alex Appleget <alexappleget2014@gmail.com>
2024-11-18 12:49:55 -0600

    Fix: created test for player table in the page.tsx

--- app/(admin)/admin/players/page.test.tsx
+++ app/(admin)/admin/players/page.test.tsx
@@ -1,5 +1,26 @@
-// /Users/ryanfurrer/Developer/GitHub/gridiron-survivor/app/(admin)/admin/notifications/page.test.tsx
+import { render, screen, waitFor } from "@testing-library/react";
+import AdminPlayers from "./page";
+import { getAllLeagueEntries } from "@/api/apiFunctions";
+
+jest.mock('@api/apiFunctions', () => ({
+    getAllLeagueEntries: jest.fn(),
+}));
 
 describe('Admin players page', () => {
-  
+  render(<AdminPlayers />);
+
+  const mockGetAllLeagueEntries = getAllLeagueEntries as jest.Mock;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should render the Player data table component', async () => {
+    mockGetAllLeagueEntries.mockResolvedValue({});
+
+    await waitFor(() => {
+        const playerTable = screen.getByTestId('data-table');
+        expect(playerTable).toBeInTheDocument();
+    })
+  })
 });
